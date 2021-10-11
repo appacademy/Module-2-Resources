a204810ff87c147295e057571a2a68d469c9a101
alex <aapezzati@gmail.com>
2021-10-11 13:36:55 -0400

    w4d4 content reset

--- week-4/d4-test-driven-development/practices/practice-for-week-04-error-handling-main/error-handling.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-error-handling-main/error-handling.js
@@ -1,22 +1,8 @@
 // 1.
 function sum(array) {
   let sum = 0;
-
-  try {
-    // if (array instanceof Array) {
-      
-      for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-      }
-    // } else {
-      console.log('must be an array')
-    // }
-  } catch (e) {
-    if (e instanceof TypeError) {
-      console.log(e.name);
-    } else {
-      throw e;
-    }
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
   return sum;
 }
@@ -25,21 +11,10 @@ let res = sum(null);
 console.log(res);
 
 // 2.
-
-function sayName(name) {
-  if (!(typeof name === "string")) {
-    throw new TypeError("Invalid name! Must be a string!");
-  }
-
-  console.log(name);
-}
-
-try {
-  sayName("Alex");
-  sayName(1);
-} catch (error) {
-  console.log("Oops, something went wrong: ", error.message);
-}
+// tests
+sayName("Alex");
+sayName(1);
+// Your code here
 
 // 3.
 function greet(greeting) {
@@ -48,11 +23,4 @@ function greet(greeting) {
   }
 
   console.log(greeting);
-}
-
-try {
-  greet("Hola!");
-  greet();
-} catch (e) {
-  console.log("Hello World!");
 }
--- week-4/d4-test-driven-development/practices/practice-for-week-04-unit-testing-practice-main/funcs.js
+++ week-4/d4-test-driven-development/practices/practice-for-week-04-unit-testing-practice-main/funcs.js
@@ -1,23 +1,13 @@
 function isFive(num) {
   // Your code here
-  return num === 5;
 }
 
 function isOdd(number) {
   // Your code here
-  if (typeof number !== 'number') {
-    throw Error('not a number')
-  }
-  return number % 2 !== 0;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
-  const array = [];
-  for (let i = min; i <= max; i += step) {
-    array.push(i);
-  }
-  return array;
 }
 
 
