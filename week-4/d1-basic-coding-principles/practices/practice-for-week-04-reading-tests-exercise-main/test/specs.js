d47c58dd816113189da365aae41065fb0dbd670f
alex <aapezzati@gmail.com>
2021-10-11 17:04:06 -0400

    solution for w4d1 reading tests

--- week-4/d1-basic-coding-principles/practices/practice-for-week-04-reading-tests-exercise-main/test/specs.js
+++ week-4/d1-basic-coding-principles/practices/practice-for-week-04-reading-tests-exercise-main/test/specs.js
@@ -4,12 +4,14 @@ const { isFive, isOdd, myRange } = require("../funcs");
 
 describe("isFive(num)", () => {
   it("should return true if the num is 5 otherwise false", () => {
+    // checking if isFive(5) results in true
     const num1 = 5;
 
     const actual1 = isFive(num1); // should be true
 
     expect(actual1).to.be.true;
 
+    // checking if isFive(4) and isFive("not 5") results in false
     const otherInput = "not 5";
     const num2 = 4;
 
@@ -23,6 +25,7 @@ describe("isFive(num)", () => {
 
 describe("isOdd(number)", () => {
   it("should return true if number is odd", () => {
+    // checking if isOdd(3), isOdd(2953), and isOdd(-999) results in true
     const num1 = 3;
     const num2 = 2953;
     const num3 = -999;
@@ -37,6 +40,7 @@ describe("isOdd(number)", () => {
   });
 
   it("should return false if the num is even", () => {
+    // checking if isOdd(4), isOdd(2952), and isOdd(-998) results in false
     const num1 = 4;
     const num2 = 2952;
     const num3 = -998;
@@ -51,6 +55,8 @@ describe("isOdd(number)", () => {
   });
 
   it("should throw an error if num is not type of Number", () => {
+    // checking if isOdd("i am a string"), isOdd({ i: "am", an: "object" }),
+    // and isOdd(["i", "am", "an", "array"]) throws errors
     const string = "i am a string";
     const object = { i: "am", an: "object" };
     const array = ["i", "am", "an", "array"];
@@ -62,8 +68,11 @@ describe("isOdd(number)", () => {
 });
 
 describe("myRange(min, max, step)", () => {
+  // when the step parameter is not provided
   context("if step is not provided", () => {
     it("should return the correct array with default value step=1", () => {
+      // checking if isOdd("i am a string"), isOdd({ i: "am", an: "object" }),
+      // and isOdd(["i", "am", "an", "array"]) throws errors
       const [min1, max1] = [0, 5];
       const [min2, max2] = [6, 3];
 
@@ -77,8 +86,12 @@ describe("myRange(min, max, step)", () => {
     });
   });
 
-  context("if step is provided", () => { 
+  context("if step is provided", () => {
+    // when the step parameter is provided
     it("should return the correct array", () => {
+      // checking if myRange(0, 5, 1) results in [0, 1, 2, 3, 4, 5]
+      // checking if myRange(0, 5, 2) results in [0, 2, 4]
+      // checking if myRange(9, 5, 2) results in []
       const [min1, max1, step1] = [0, 5, 1];
       const [min2, max2, step2] = [0, 5, 2];
       const [min3, max3, step3] = [9, 5, 2];
