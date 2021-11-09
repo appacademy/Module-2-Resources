3affc715e9ced54bebc89d85391e3e90dab40769
alex <aapezzati@gmail.com>
2021-11-09 15:59:08 -0500

    inheritance practice solutions

--- week-4/d2-es6-classes/practices/inheritance/classes/student.js
+++ week-4/d2-es6-classes/practices/inheritance/classes/student.js
@@ -1,12 +1,30 @@
 const Person = require('./person');
 
-// Your code here
+class Student extends Person {
+    constructor(firstName, lastName, major, GPA) {
+        super(firstName, lastName);
+        this.major = major;
+        this.GPA = GPA;
+    }
 
+    static compareGPA(student1, student2) {
+        if (student1.GPA > student2.GPA) {
+            return `${student1.firstName} ${student1.lastName} has the higher GPA.`;
+        } else if (student2.GPA > student1.GPA) {
+            return `${student2.firstName} ${student2.lastName} has the higher GPA.`;
+        } else {
+            return `Both students have the same GPA.`;
+        }
+    }
+}
+
+// const alex = new Student('alex', 'p', 'science', 1000);
+// console.log(alex);
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Student;
+    module.exports = Student;
 } catch {
-  module.exports = null;
+    module.exports = null;
 }
--- week-4/d2-es6-classes/practices/inheritance/classes/teacher.js
+++ week-4/d2-es6-classes/practices/inheritance/classes/teacher.js
@@ -1,12 +1,32 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person {
+    constructor(firstName, lastName, subject, yearsOfExperience) {
+        super(firstName, lastName);
+        this.subject = subject;
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    static combinedYearsOfExperience(teachers) {
+        // return teachers.reduce(
+        //     (accumulator, teacher) => teacher.yearsOfExperience + accumulator,
+        //     0
+        // );
+        let totalYears = 0;
+        for (let teacher of teachers) {
+            totalYears += teacher.yearsOfExperience;
+        }
+        // console.log(teachers, 'teachers');
+        // console.log(totalYears, 'total years');
+        return totalYears;
+    }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Teacher;
+    module.exports = Teacher;
 } catch {
-  module.exports = null;
+    module.exports = null;
 }
