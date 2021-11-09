82ff9ec9f405fc01c89570906cf34a29ae0e9bd1
alex <aapezzati@gmail.com>
2021-11-09 12:28:15 -0500

    common js modules practice solutions

--- week-4/d2-es6-classes/practices/common-js-modules/index.js
+++ week-4/d2-es6-classes/practices/common-js-modules/index.js
@@ -1,10 +1,19 @@
+const sayHelloTo = require('./send-messages/say-hello-to');
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
 
-// Your code here
+const { msg1, msg2, msg3 } = require('./messages/index');
 
+// const {
+//     message1: msg1,
+//     message2: msg2,
+//     message3: msg3,
+// } = require('./messages/index');
+
+// const [msg1, msg2, msg3] = require('./messages/index');
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
-sayHelloTo("Mr. Potato");
+sayHelloTo('Mr. Potato');
 giveMessageToMrsPotato(msg1);
 giveMessageToMrsPotato(msg2);
 giveMessageToMrsPotato(msg3);
