93dfed2f461628b9351775cbf406279237555e03
Kristen Chauncey <chaunceykristen@gmail.com>
2021-11-10 11:20:16 -0600

    add day 3 bind solution

--- week-4/d3-context-bind-call-apply/practices/bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/bind/test.js
@@ -2,12 +2,12 @@
 const Employee = require("./employee");
 
 let john = new Employee("John Wick", "Dog Lover");
-let saySomething = john.sayName
-setTimeout(saySomething, 2000);
+
+// setTimeout(john.sayName, 2000);
 // setTimeout(john.sayOccupation, 3000);
 
-// let sayNameBound = john.sayName.bind(john);
-// let sayOccupationBound = john.sayOccupation.bind(john);
+let sayNameBound = john.sayName.bind(john)
+let sayOccupationBound = john.sayOccupation.bind(john)
 
-// setTimeout(sayNameBound, 2000);
-// setTimeout(sayOccupationBound, 3000);
+setTimeout(sayNameBound, 2000)
+setTimeout(sayOccupationBound, 3000)
