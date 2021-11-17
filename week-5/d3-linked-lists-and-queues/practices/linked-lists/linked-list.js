ad3a693fb64656bd312b34594f7134ec65415974
southwestmogrown <mail@southwestmogrown.com>
2021-11-17 11:24:04 -0600

    Week 5 day 2 morning lecture through single linked list

--- week-5/d3-linked-lists-and-queues/practices/linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/linked-lists/linked-list.js
@@ -8,14 +8,31 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);
+    this.length++;
+    if (!this.head) {
+      this.head = newNode;
+      return this;
+    }
+    let curr = this.head;
+    while (curr.next) {
+      curr = curr.next
+    }
+    curr.next = newNode;
   }
 
   // You can use this function to help debug
