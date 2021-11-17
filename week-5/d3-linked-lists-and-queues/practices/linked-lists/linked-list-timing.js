8fd43617b3b4aaead0676a3bd5a451304239f140
alex <aapezzati@gmail.com>
2021-11-17 13:34:39 -0500

    doublely linked list walkthrough

--- week-5/d3-linked-lists-and-queues/practices/linked-lists/linked-list-timing.js
+++ week-5/d3-linked-lists-and-queues/practices/linked-lists/linked-list-timing.js
@@ -6,4 +6,15 @@ Construct a timing test to verify the time complexities of `addToHead` and
 `addToTail` for both singly and doubly linked lists.
 */
 
-// Your code here
+const sll = new LinkedList()
+const dll = new DoublyLinkedList()
+
+const n = 10000
+
+// timer start
+for (let i = 0; i < n; i++ ){
+    sll.addToHead(i)
+}
+// timer end
+
+dll.addToHead(2)
