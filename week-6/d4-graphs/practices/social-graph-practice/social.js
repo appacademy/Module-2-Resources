bf13bf89d2ac29af57c73c45fa2ff4936e130104
Kristen Chauncey <chaunceykristen@gmail.com>
2021-11-29 13:03:13 -0600

    add social graph practice solution

--- week-6/d4-graphs/practices/social-graph-practice/social.js
+++ week-6/d4-graphs/practices/social-graph-practice/social.js
@@ -1,6 +1,5 @@
 // Implement the SocialNetwork class here
 class SocialNetwork {
-
   constructor() {
     this.users = {};
     this.follows = {};
@@ -8,28 +7,83 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    //!!START
+    this.currentID++;
+    const nextID = this.currentID;
+
+    this.users[nextID] = { id: nextID, name: name };
+    this.follows[nextID] = new Set();
+
+    return nextID;
+    //!!END
   }
 
   getUser(userID) {
-    // Your code here
+    //!!START
+    if (this.users[userID]) return this.users[userID];
+    else return null;
+    //!!END
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    //!!START
+    if (!this.users[userID1] || !this.users[userID2]) {
+      return false;
+    }
+
+    this.follows[userID1].add(userID2);
+    return true;
+    //!!END
   }
 
   getFollows(userID) {
-    // Your code here
+    //!!START
+    return this.follows[userID];
+    //!!END
   }
 
   getFollowers(userID) {
-    // Your code here
+    //!!START
+    const followers = new Set();
+
+    for (let id in this.follows) {
+      if (this.follows[id].has(userID)) followers.add(parseInt(id));
+    }
+
+    return followers;
+    //!!END
   }
 
   getRecommendedFollows(userID, degrees) {
-    // Your code here
+    //!!START
+    let queue = [[userID]];
+
+    let recommended = [];
+
+    let visited = new Set();
+    visited.add(userID);
+
+    while (queue.length > 0) {
+      let path = queue.shift();
+      let id = path[path.length - 1];
+
+      if (path.length > degrees + 2) break;
+
+      if (path.length > 2) recommended.push(id);
+
+      for (let follow of this.follows[id]) {
+        if (!visited.has(follow)) {
+          visited.add(follow);
+          // Use concat to create a new array
+          // DON'T modify the existing path!!!
+          queue.push(path.concat([follow]));
+        }
+      }
+    }
+
+    return recommended;
   }
+  //!!END
 }
 
 module.exports = SocialNetwork;
