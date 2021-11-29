d57fde262262d97f371b8ceb0b107ab98d0d52ff
Kristen Chauncey <chaunceykristen@gmail.com>
2021-11-29 11:06:54 -0600

    add bfs solution

--- week-6/d4-graphs/practices/repl-problem-solutions/simple-breadth-first-search-practice-solution.js
+++ week-6/d4-graphs/practices/repl-problem-solutions/simple-breadth-first-search-practice-solution.js
@@ -9,6 +9,26 @@ const adjList = {
 
 function breadthFirstSearch(start, end) {
   // your code here
+  let queue = [];
+  let visited = new Set();
+
+  queue.push(start)
+  visited.add(start)
+
+  while (queue.length) {
+    let currentNode = queue.shift();
+    if (currentNode === end) {
+      //found it!
+      return true;
+    }
+    adjList[currentNode].forEach(neighbor => {
+      if (!visited.has(neighbor)) {
+        queue.push(neighbor)
+        visited.add(neighbor)
+      }
+    })
+  }
+  return false;
 }
 
 console.log("First Test:");
