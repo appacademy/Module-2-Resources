c1f089f8d94f838062fcf9dd09dbe019afa6e31d
Kristen Chauncey <chaunceykristen@gmail.com>
2021-12-13 16:32:23 -0600

    add timing code solution

--- week-5/d1-Big-O/practices/timing-benchmarks/timing-benchmarks-pt1.js
+++ week-5/d1-Big-O/practices/timing-benchmarks/timing-benchmarks-pt1.js
@@ -1,12 +1,22 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
   // Fill this in
+  let total = 0;
+  for (let i = 1; i <= n; i++){
+    total += i;
+  }
+  return total;
 }
 
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
   // Fill this in
+  let total = 0;
+  for (let i = 1; i <= n; i++){
+    total += addNums(i);
+  }
+  return total;
 }
 
 
