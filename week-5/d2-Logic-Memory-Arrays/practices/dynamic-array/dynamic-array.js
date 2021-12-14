945d155ce7d006b557e68876b0d725fb19a1b670
alex pezzati <aapezzati@gmail.com>
2021-12-14 14:14:28 -0500

    dynamic array practice solution

--- week-5/d2-Logic-Memory-Arrays/practices/dynamic-array/dynamic-array.js
+++ week-5/d2-Logic-Memory-Arrays/practices/dynamic-array/dynamic-array.js
@@ -1,25 +1,28 @@
 class DynamicArray {
-
-    constructor(defaultSize=4) {
-
-      // Fill this out
-
+    constructor(defaultSize = 4) {
+        this.length = 0;
+        this.capacity = defaultSize;
+        this.data = new Array(this.capacity);
+        this.data = [];
     }
 
     read(index) {
-
-      // Fill this out
-      // You may not use any built-in JS array functions
+        // console.log(this.data);
+        // console.log(this.data[index]);
+        if (index >= this.length) return undefined;
+        return this.data[index];
+        // You may not use any built-in JS array functions
     }
 
     unshift(val) {
+        for (let i = this.length; i > 0; i--) {
+            this.data[i] = this.data[i - 1];
+        }
 
-      // Fill this out
-      // You may not use any built-in JS array functions
-
+        this.data[0] = val;
+        this.length++;
+        // You may not use any built-in JS array functions
     }
+}
 
-  }
-
-
-  module.exports = DynamicArray;
+module.exports = DynamicArray;
