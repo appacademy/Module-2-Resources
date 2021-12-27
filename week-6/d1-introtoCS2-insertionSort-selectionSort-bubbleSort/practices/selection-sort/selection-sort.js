eec4c923b0abfc31fb8373a953f0e50823493728
alex pezzati <aapezzati@gmail.com>
2021-12-27 18:10:01 -0500

    selection sort practice setup

--- week-6/d1-introtoCS2-insertionSort-selectionSort-bubbleSort/practices/selection-sort/selection-sort.js
+++ week-6/d1-introtoCS2-insertionSort-selectionSort-bubbleSort/practices/selection-sort/selection-sort.js
@@ -1,34 +1,27 @@
-
-
 function selectionSort(arr) {
+    // Copy the original array
 
-  // Copy the original array
+    // Create an array to store the sorted values
 
-  // Create an array to store the sorted values
-
-  // While the array is not empty...
+    // While the array is not empty...
 
     // Do not move this console.log
-    console.log(sorted.join(","));
+    console.log(sorted.join(','));
 
     // Find the index of the minimum value in the unsorted half
 
     // Save and remove the value at the min index
 
     // Add the min value to the end of the sorted array
-
 }
 
-
-
 function selectionSortInPlace(arr) {
+    // Set a pointer at zero diving the array into sorted and unsorted halves
 
-  // Set a pointer at zero diving the array into sorted and unsorted halves
-
-  // Repeat while the unsorted half is not empty:
+    // Repeat while the unsorted half is not empty:
 
     // Do not move this console.log
-    console.log(arr.join(","));
+    console.log(arr.join(','));
 
     // Find the index of the minimum value in the unsorted half
 
@@ -39,8 +32,6 @@ function selectionSortInPlace(arr) {
     // Put the min value at the divider
 
     // Increment the divider and repeat
-
 }
-
 
 module.exports = [selectionSort, selectionSortInPlace];
