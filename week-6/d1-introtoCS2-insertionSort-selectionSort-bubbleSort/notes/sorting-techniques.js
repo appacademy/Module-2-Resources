f6c95b00149df681f12e917cb63e5b2459490fa5
alex pezzati <aapezzati@gmail.com>
2021-12-27 16:30:49 -0500

    bubble sort practice complete

--- week-6/d1-introtoCS2-insertionSort-selectionSort-bubbleSort/notes/sorting-techniques.js
+++ week-6/d1-introtoCS2-insertionSort-selectionSort-bubbleSort/notes/sorting-techniques.js
@@ -26,24 +26,24 @@ console.log(arr); // [0, 2, 1, 3, 6, 5, 4]
 // //
 // // SHIFTING
 // //
-// const arr1 = [0, 1, 2, 3, 4, 5];
-// const arr2 = [0, 1, 2, 3, 4, 5];
+const arr1 = [0, 1, 2, 3, 4, 5];
+const arr2 = [0, 1, 2, 3, 4, 5];
 
-// // If you want to shift the array to the right by 1...
+// If you want to shift the array to the right by 1...
 
-// // Shifting from front to back will overwrite all values
-// for (let i = 1; i < arr1.length; i++) {
-//     arr1[i] = arr1[i - 1];
-// }
+// Shifting from front to back will overwrite all values
+for (let i = 1; i < arr1.length; i++) {
+    arr1[i] = arr1[i - 1];
+}
 
-// console.log(arr1); // [ 0, 0, 0, 0, 0, 0 ]
+console.log(arr1); // [ 0, 0, 0, 0, 0, 0 ]
 
-// // Instead, start from the back and shift backwards
-// for (let i = arr2.length - 1; i > 0; i--) {
-//     arr2[i] = arr2[i - 1];
-// }
+// Instead, start from the back and shift backwards
+for (let i = arr2.length - 1; i > 0; i--) {
+    arr2[i] = arr2[i - 1];
+}
 
-// console.log(arr2); // [ 0, 0, 1, 2, 3, 4 ]
+console.log(arr2); // [ 0, 0, 1, 2, 3, 4 ]
 
 // //
 // //
--- week-6/d1-introtoCS2-insertionSort-selectionSort-bubbleSort/practices/bubble-sort/bubble-sort.js
+++ week-6/d1-introtoCS2-insertionSort-selectionSort-bubbleSort/practices/bubble-sort/bubble-sort.js
@@ -1,15 +1,28 @@
 function bubbleSort(arr) {
-    // Iterate through the array
+    let swapped = true;
 
-    // If the current value is greater than its neighbor to the right
-    // Swap those values
+    while (swapped) {
+        swapped = false;
 
-    // Do not move this console.log
-    console.log(arr.join(','));
+        // Iterate through the array
+        for (let i = 0; i < arr.length; i++) {
+            // If the current value is greater than its neighbor to the right
+            // Swap those values
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
 
-    // If you get to the end of the array and no swaps have occurred, return
+                swapped = true;
 
-    // Otherwise, repeat from the beginning
+                // Do not move this console.log
+                console.log(arr.join(','));
+            }
+        }
+
+        // If you get to the end of the array and no swaps have occurred, return
+        if (swapped === false) return arr;
+
+        // Otherwise, repeat from the beginning
+    }
 }
 
 module.exports = bubbleSort;
