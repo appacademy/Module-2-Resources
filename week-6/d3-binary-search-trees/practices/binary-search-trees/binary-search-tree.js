95e0bd11194590d60a7b074bef3feb6c0b9ea96f
alex pezzati <aapezzati@gmail.com>
2021-12-28 12:24:09 -0500

    binary search tree part 1 complete

--- week-6/d3-binary-search-trees/practices/binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/binary-search-trees/binary-search-tree.js
@@ -1,50 +1,63 @@
 // Do not change this
 class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
 
-  constructor() {
-    // Your code here
-  }
+    insert(val, currentNode = this.root) {
+        if (!this.root) {
+            this.root = new TreeNode(val);
+            return;
+        }
 
-  insert(val, currentNode=this.root) {
-    // Your code here
-  }
+        if (val < currentNode.val) {
+            if (!currentNode.left) {
+                currentNode.left = new TreeNode(val);
+            } else {
+                this.insert(val, currentNode.left);
+            }
+        } else {
+            if (!currentNode.right) {
+                currentNode.right = new TreeNode(val);
+            } else {
+                this.insert(val, currentNode.right);
+            }
+        }
+    }
 
-  search(val) {
-    // Your code here
-  }
+    search(val) {
+        // Your code here
+    }
 
+    preOrderTraversal(currentNode = this.root) {
+        // Your code here
+    }
 
-  preOrderTraversal(currentNode = this.root) {
-    // Your code here
-  }
+    inOrderTraversal(currentNode = this.root) {
+        // Your code here
+    }
 
-
-  inOrderTraversal(currentNode = this.root) {
-    // Your code here
-  }
-
-
-  postOrderTraversal(currentNode = this.root) {
-    // Your code here
-  }
+    postOrderTraversal(currentNode = this.root) {
+        // Your code here
+    }
 
     // Breadth First Traversal - Iterative
-  breadthFirstTraversal() {
-    // Your code here
-  }
+    breadthFirstTraversal() {
+        // Your code here
+    }
 
-  // Depth First Traversal - Iterative
-  depthFirstTraversal() {
-    // Your code here
-  }
+    // Depth First Traversal - Iterative
+    depthFirstTraversal() {
+        // Your code here
+    }
 }
 
 module.exports = { BinarySearchTree, TreeNode };
