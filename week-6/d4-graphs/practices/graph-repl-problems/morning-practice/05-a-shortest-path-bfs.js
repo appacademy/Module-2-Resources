e521af390d62ee7cb2cc96b6a2f93fd8dc44fa38
alex pezzati <aapezzati@gmail.com>
2021-12-29 10:42:55 -0500

    added instructions for graph repl problems

--- week-6/d4-graphs/practices/graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
+++ week-6/d4-graphs/practices/graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
@@ -1,19 +1,28 @@
+/*
+One of the most common uses of a breadth-first search is to find a
+shortest path from one node to another.
+Copy and paste your breadth-first search here and refactor it to
+use the technique from the reading.
+If it is possible to reach end from start,
+return a shortest path.  Otherwise, return false.
+*/
+
 const adjList = {
     1: [2, 5],
     2: [1, 3, 5],
     3: [2, 4],
     4: [3, 5],
     5: [1, 2, 4],
-    6: []
-}
+    6: [],
+};
 
 function aShortestPath(start, end) {
-  // your code here
+    // your code here
 }
 
-console.log("First Test:");
+console.log('First Test:');
 console.log(aShortestPath(1, 3)); // -> [ 1, 2, 3 ] (One possible solution)
-console.log("Second Test:");
+console.log('Second Test:');
 console.log(aShortestPath(4, 1)); // -> [ 4, 5, 1 ] (One possible solution)
-console.log("Third Test:");
+console.log('Third Test:');
 console.log(aShortestPath(6, 1)); // -> false
--- week-6/d4-graphs/practices/graph-repl-problems/morning-practice/06-degress-of-separation-bfs.js
+++ week-6/d4-graphs/practices/graph-repl-problems/morning-practice/06-degress-of-separation-bfs.js
@@ -1,19 +1,25 @@
+/*
+Another common question is the degrees of separation between two nodes.
+In other words, what is the shortest number of steps you
+can take to get from one to another.
+*/
+
 const adjList = {
     1: [2, 5],
     2: [1, 3, 5],
     3: [2, 4],
     4: [3, 5],
     5: [1, 2, 4],
-    6: []
-}
+    6: [],
+};
 
 function degreesOfSeparation(start, end) {
-  // your code here
+    // your code here
 }
 
-console.log("First Test:");
+console.log('First Test:');
 console.log(degreesOfSeparation(1, 3)); // -> 2
-console.log("Second Test:");
+console.log('Second Test:');
 console.log(degreesOfSeparation(5, 2)); // -> 1
-console.log("Third Test:");
+console.log('Third Test:');
 console.log(degreesOfSeparation(6, 1)); // -> false
