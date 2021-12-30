b14d0313d21c2c9aa2fda318e91faa0ce9ad6779
alex pezzati <aapezzati@gmail.com>
2021-12-30 11:37:29 -0500

    social graph practice setup

--- week-6/d4-graphs/practices/social-graph-practice/social.js
+++ week-6/d4-graphs/practices/social-graph-practice/social.js
@@ -1,35 +1,40 @@
 // Implement the SocialNetwork class here
 class SocialNetwork {
+    constructor() {
+        this.users = {};
+        this.follows = {};
+        this.currentID = 0;
+    }
 
-  constructor() {
-    this.users = {};
-    this.follows = {};
-    this.currentID = 0;
-  }
+    addUser(name) {
+        this.currentID++;
+        const nextID = this.currentID;
 
-  addUser(name) {
-    // Your code here
-  }
+        this.users[nextID] = { id: nextID, name: name };
+        // this.follows[nextID] = new Set();
 
-  getUser(userID) {
-    // Your code here
-  }
+        return nextID;
+    }
 
-  follow(userID1, userID2) {
-    // Your code here
-  }
+    getUser(userID) {
+        // Your code here
+    }
 
-  getFollows(userID) {
-    // Your code here
-  }
+    follow(userID1, userID2) {
+        // Your code here
+    }
 
-  getFollowers(userID) {
-    // Your code here
-  }
+    getFollows(userID) {
+        // Your code here
+    }
 
-  getRecommendedFollows(userID, degrees) {
-    // Your code here
-  }
+    getFollowers(userID) {
+        // Your code here
+    }
+
+    getRecommendedFollows(userID, degrees) {
+        // Your code here
+    }
 }
 
 module.exports = SocialNetwork;
