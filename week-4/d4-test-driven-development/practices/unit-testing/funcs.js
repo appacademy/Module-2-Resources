88ec888f7845e742256b65a9a874c39a8e331fa4
Kristen Chauncey <chaunceykristen@gmail.com>
2022-01-13 15:09:10 -0600

    add solutions

--- week-4/d4-test-driven-development/practices/unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/unit-testing/funcs.js
@@ -1,13 +1,23 @@
 function isFive(num) {
   // Your code here
+  return num === 5;
 }
 
 function isOdd(number) {
   // Your code here
+  if (typeof number !== 'number') {
+    throw Error('oopsy')
+  }
+  return number % 2 !== 0;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  const array = []
+  for (let i = min; i <= max; i += step){
+    array.push(i)
+  }
+  return array;
 }
 
 
