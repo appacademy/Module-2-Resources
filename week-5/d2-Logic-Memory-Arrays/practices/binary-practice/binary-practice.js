c18df02982d3919d477d533102ebca2edbc0bc41
alex pezzati <aapezzati@gmail.com>
2022-01-18 22:12:16 -0500

    moved and made js for binary-practice

---
+++ week-5/d2-Logic-Memory-Arrays/practices/binary-practice/binary-practice.js
@@ -0,0 +1,55 @@
+//  Binary, number base practice
+// In this exercise, translate the following values into the appropriate number base
+
+//  Binary to base 10
+
+0b1010
+0b0011
+
+//  Binary to hexadecimal
+
+0b1010
+0b0011
+
+//  Hexadecimal to base 10
+
+0xa1
+0xff
+
+//  Hexadecimal to binary
+
+0xa1
+0xff
+
+//  Base 10 to binary
+
+8
+24
+255
+
+//  Base 10 to hexadecimal
+
+8
+24
+255
+
+//  Base 10 to ASCII
+
+65
+66
+97
+98
+
+//  Hexadecimal to ASCII
+
+0x41
+0x42
+0x61
+0x62
+
+//  Binary to ASCII
+
+0b01000001
+0b01000010
+0b01100001
+0b01100010
