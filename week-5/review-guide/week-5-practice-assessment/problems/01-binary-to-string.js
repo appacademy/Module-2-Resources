7eb3ba5936585fb3bbb7ee0274a9fb36963c41bc
alex pezzati <aapezzati@gmail.com>
2022-01-23 16:19:40 -0500

    week 5 practice assessment walkthrough solution

--- week-5/review-guide/week-5-practice-assessment/problems/01-binary-to-string.js
+++ week-5/review-guide/week-5-practice-assessment/problems/01-binary-to-string.js
@@ -1,10 +1,20 @@
 function binaryToString(binaryBlob) {
-    // Your code here
+    let asciiStr = ''
+
+    for (let i = 0; i < binaryBlob.length; i += 8) {
+        const binary = binaryBlob.slice(i, i + 8)
+        const charCode = parseInt(binary, 2)
+        const ascii = String.fromCharCode(charCode)
+        asciiStr += ascii
+        // console.log(binaryBlob, binary, charCode, ascii, asciiStr)
+    }
+
+    return asciiStr
 }
 
 // Uncomment to run local tests
 
-// console.log(binaryToString('010000010100001001000011')); // 'ABC'
-// console.log(binaryToString('001101100011011100111000')); // '678'
+console.log(binaryToString('010000010100001001000011')); // 'ABC'
+console.log(binaryToString('001101100011011100111000')); // '678'
 
 module.exports = binaryToString;
