5b9106192993868c43ea6e1679c71b4ac374930a
alex pezzati <aapezzati@gmail.com>
2022-01-28 16:32:40 -0500

    tree problem solution

--- week-6/review-guide/week-6-practice-assessment-tree-problem-solution/tree_practice.js
+++ week-6/review-guide/week-6-practice-assessment-tree-problem-solution/tree_practice.js
@@ -1,8 +1,8 @@
 class TreeNode {
   constructor(value, left, right) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
+      this.value = value;
+      this.left = left;
+      this.right = right;
   }
 }
 
@@ -23,13 +23,80 @@ class TreeNode {
 
 // Expected Output -> [ 5, 7, 3, 9, 4 ]
 
+//
+// Three solutions with different approaches are provided below
+//
+
 function findMaxEachLevel(root) {
-  // Your code here
+  if (!root) return [];
+  const result = [];
+  let queue = [];
+  queue.push(root);
+  while (queue.length) {
+      const len = queue.length;
+
+      // Keep track of the max per level
+      let max = -Infinity;
+      for (let i = 0; i < len; i++) {
+          const current = queue.shift();
+          max = Math.max(max, current.value);
+          if (current.left) queue.push(current.left);
+          if (current.right) queue.push(current.right);
+      }
+
+      // Add the max to the result array
+      result.push(max);
+  }
+  return result;
 }
+
+// // recursion solution
+// function findMaxEachLevel(root) {
+//   function recursionHelper(root, result, depth) {
+//       if (!root) return;
+
+//       if (result.length === depth) {
+//           result.push(root.value);
+//       } else {
+//           result[depth] = Math.max(root.value, result[depth]);
+//       }
+
+//       recursionHelper(root.left, result, depth + 1);
+//       recursionHelper(root.right, result, depth + 1);
+//   }
+
+//   recursionHelper(root, (result = []), (depth = 0));
+//   return result;
+// }
+
+// // counter solution
+// function findMaxEachLevel(root) {
+//   const queue = [root];
+//   let count = 1;
+//   let currentLevel = [];
+//   let values = [];
+
+//   while (queue.length) {
+//       const currentNode = queue.shift();
+//       currentLevel.push(currentNode.value);
+//       count--;
+
+//       if (currentNode.left) queue.push(currentNode.left);
+//       if (currentNode.right) queue.push(currentNode.right);
+
+//       if (!count) {
+//           values.push(Math.max(...currentLevel));
+//           currentLevel = [];
+//           count = queue.length;
+//       }
+//   }
+
+//   return values;
+// }
 
 // Uncomment the code below for local testing.
 
-// // Build a tree for testing
+// Build a tree for testing
 
 //   const simpleTree = new TreeNode(4, null, null);
 //   simpleTree.right = new TreeNode(1, null, null);
@@ -40,8 +107,8 @@ function findMaxEachLevel(root) {
 // console.log(findMaxEachLevel(simpleTree)); // -> [ 4, 3, 2 ]
 
 /*******************************************************************************
- * Do not change the code after this line.
- */
+* Do not change the code after this line.
+*/
 
 try {
   exports.TreeNode = TreeNode;
