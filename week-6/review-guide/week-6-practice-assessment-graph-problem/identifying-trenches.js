e524347bd43c4237f5e926b5b0801c2fd6b388f8
alex pezzati <aapezzati@gmail.com>
2022-01-28 16:25:21 -0500

    week 6 practice assessment setup

---
+++ week-6/review-guide/week-6-practice-assessment-graph-problem/identifying-trenches.js
@@ -0,0 +1,70 @@
+// HINT:  Review counting islands before starting this problem!
+
+function findNeighbors(node, matrix) {
+    // Only consider N, S, E, W nodes
+
+    // North
+
+    // South
+
+    // East
+
+    // West
+
+    // Your code here
+}
+
+function trenchTraversal(node, matrix, visited) {
+    // Don't bother traversing if it is to shallow
+
+    // Traverse the potential trench to count it's length
+    // Your code here
+}
+
+function identifyTrench(trenchMatrix) {
+    // Start at 0,0 and attempt to traverse any unvisited nodes
+    // Your code here
+}
+
+// Uncomment for local testing
+
+// // Example 0
+// const sonar_0 = [
+//     [-5, -5, -5],
+//     [-6, -5, -8],
+//     [-5, -7, -5]
+// ]
+
+// console.log(findNeighbors([1,1], sonar_0)) => Expect [[2, 1], [1, 0], [1, 2]];
+
+// // Example 1
+// const sonar_1 = [
+//           [-5,-5,-5,-5,-5],
+//           [-5,-8,-8,-9,-7],
+//           [-5,-5,-5,-5,-8],
+//           [-5,-5,-5,-5,-5]
+// ]
+// console.log(identifyTrench(sonar_1)) // <- Expect 'true'
+
+// // Example 2
+// const sonar_2 = [
+//           [-5,-5,-5,-7,-5],
+//           [-5,-8,-8,-9,-5],
+//           [-5,-5,-5,-7,-5],
+//           [-5,-5,-5,-5,-5]
+// ]
+// console.log(identifyTrench(sonar_2)) // <- Expect 'false'
+
+// // Example 3
+// const sonar_3 = [
+//           [-5,-5,-5,-5,-5],
+//           [-5,-5,-5,-5,-5],
+//           [-5,-9,-9,-5,-5],
+//           [-5,-5,-5,-5,-5]
+// ]
+// console.log(identifyTrench(sonar_3)) // <- Expect 'false'
+
+
+/*************DO NOT MODIFY UNDER THIS LINE ***************/
+
+module.exports = [identifyTrench, findNeighbors, trenchTraversal];
