71a47568df009dc95dc11f227eff60751653cf66
alex pezzati <aapezzati@gmail.com>
2022-01-30 17:05:42 -0500

    graph problem solution

--- week-6/review-guide/week-6-practice-assessment-graph-problem-solution/identifying-trenches.js
+++ week-6/review-guide/week-6-practice-assessment-graph-problem-solution/identifying-trenches.js
@@ -2,28 +2,90 @@
 
 function findNeighbors(node, matrix) {
     // Only consider N, S, E, W nodes
+    const [row, col] = node
+
+    const neighbors = []
+    // const row = node[0]
+    // const col = node[1]
+
+    //  north east
+    // row - 1, col + 1
+    // if(row - 1 >= 0 && col + 1 ) { // defensive coding
+    //     if (matrix[row - 1][col + 1]) console.log([row - 1, col + 1])
+    // }
 
     // North
-
+    if (row - 1 >= 0) { // defensive coding
+        if (matrix[row - 1][col] < -5) neighbors.push([row - 1, col])
+    }
+    // matrix[-1] NEVER SHOULD HAPPEN
     // South
+    if (row + 1 < matrix.length) {
+        if (matrix[row + 1][col] < -5) neighbors.push([row + 1, col])
+    }
 
     // East
+    if (col + 1 < matrix[row].length && matrix[row][col + 1] < -5) {
+        neighbors.push([row, col + 1])
+    }
 
     // West
+    if (col - 1 >= 0 && matrix[row][col - 1] < - 5) neighbors.push([row, col - 1])
 
-    // Your code here
+   return neighbors
 }
 
 function trenchTraversal(node, matrix, visited) {
     // Don't bother traversing if it is to shallow
+    const [row, col] = node
+    if (matrix[row][col] >= -5) return false
 
     // Traverse the potential trench to count it's length
-    // Your code here
+    const stack = [node] // [[]]
+    visited.add(node.toString())
+
+    let trenchLength = 0
+
+    while(stack.length) {
+        const currentNode = stack.pop()
+
+        trenchLength++
+
+        const neighbors = findNeighbors(currentNode, matrix)
+
+        if (neighbors.length > 2 || neighbors.length < 1) return false
+
+        for (const neighbor of neighbors) {
+            const neighborStr = neighbor.toString()
+            if(!visited.has(neighborStr)) {
+                visited.add(neighborStr)
+
+                stack.push(neighbor) // [r, c]
+            }
+        }
+    }
+
+    return trenchLength >= 3 // if valid it will be True
 }
 
 function identifyTrench(trenchMatrix) {
     // Start at 0,0 and attempt to traverse any unvisited nodes
-    // Your code here
+
+    const visited = new Set()
+
+    for (let row = 0; row < trenchMatrix.length; row++) {
+        for (let col = 0; col < trenchMatrix[row].length; col++) {
+            const coordinate = [row, col].toString() // 'r,c'
+
+            if (!visited.has(coordinate)) {
+                const result = trenchTraversal([row, col], trenchMatrix, visited)
+
+                if (result) return true
+            }
+        }
+    }
+
+    return false
 }
 
 // Uncomment for local testing
