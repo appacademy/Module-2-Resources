5e0371a97b9d14173e41a634ef6aaf92e92107f1
Kristen Chauncey <chaunceykristen@gmail.com>
2022-02-10 11:10:26 -0600

    add error solution

--- week-4/d4-test-driven-development/practices/error-handling/error-handling.js
+++ week-4/d4-test-driven-development/practices/error-handling/error-handling.js
@@ -1,26 +1,52 @@
-// 1.
-function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
+// // 1.
+// function sum(array) {
+//   let sum = 0;
+//   try {
+//     for (let i = 0; i < array.length; i++) {
+//       sum += array[i];
+//     }
 
-let res = sum(null);
-console.log(res);
+//   } catch (e) {
+//     if (e instanceof TypeError) {
+//       console.log(e.message);
+//     } else {
+//       throw e;
+//     }
+//   }
+//   return sum;
+// }
+
+// let res = sum(null);
+// console.log(res);
 
 // 2.
-// tests
-sayName("Alex");
-sayName(1);
+// function sayName(name) {
+//   if (!(typeof name === "string")) {
+//     throw new TypeError("Invalid name! Must be a string!");
+//   }
+//   console.log(name);
+// }
+
+// // tests
+// try {
+//   sayName("Alex");
+//   sayName(1);
+// } catch (err) {
+//   console.log("Oops, something went wrong: ", err.name, err.message);
+// }
 // Your code here
 
-// 3.
+// // 3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
 
   console.log(greeting);
+}
+try {
+  greet('Hola!')
+  greet()
+} catch (e) {
+  console.log("Hello World!", e.message)
 }
