0090aae880f7bd46d579cdabc5a6187d0048453c
Kristen Chauncey <chaunceykristen@gmail.com>
2022-02-23 12:10:20 -0600

    add bst p3 solution

--- week-6/d3-binary-search-trees/practices/binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/binary-search-trees/binary-search-tree.js
@@ -59,14 +59,26 @@ class BinarySearchTree {
 
   preOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (!currentNode) return;
+    console.log(currentNode.val);
+    this.preOrderTraversal(currentNode.left);
+    this.preOrderTraversal(currentNode.right);
   }
 
   inOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (!currentNode) return;
+    this.inOrderTraversal(currentNode.left);
+    console.log(currentNode.val);
+    this.inOrderTraversal(currentNode.right);
   }
 
   postOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (!currentNode) return;
+    this.postOrderTraversal(currentNode.left);
+    this.postOrderTraversal(currentNode.right);
+    console.log(currentNode.val);
   }
 
   // Breadth First Traversal - Iterative
