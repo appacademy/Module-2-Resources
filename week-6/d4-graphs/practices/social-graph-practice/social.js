550114db65543a8d63dfc701998a4bfaa75ba4f9
Kristen Chauncey <chaunceykristen@gmail.com>
2022-02-24 12:49:32 -0600

    add social graph p1

--- week-6/d4-graphs/practices/social-graph-practice/social.js
+++ week-6/d4-graphs/practices/social-graph-practice/social.js
@@ -1,6 +1,5 @@
 // Implement the SocialNetwork class here
 class SocialNetwork {
-
   constructor() {
     this.users = {};
     this.follows = {};
@@ -9,22 +8,38 @@ class SocialNetwork {
 
   addUser(name) {
     // Your code here
+    this.currentID++;
+    const newId = this.currentID;
+    this.users[newId] = { id: newId, name: name };
+    this.follows[newId] = new Set();
+    return newId;
   }
 
   getUser(userID) {
     // Your code here
+    if (this.users[userID]) return this.users[userID];
+    else return null;
   }
 
   follow(userID1, userID2) {
     // Your code here
+    if (!this.users[userID1] || !this.users[userID2]) return false;
+    this.follows[userID1].add(userID2);
+    return true;
   }
 
   getFollows(userID) {
     // Your code here
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
     // Your code here
+    const followers = new Set();
+    for (let id in this.follows) {
+      if (this.follows[id].has(userID)) followers.add(parseInt(id));
+    }
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
