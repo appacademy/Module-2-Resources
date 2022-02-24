5f24d2639faa13a95bb5bdec1bb4c3d7f3566fbc
Kristen Chauncey <chaunceykristen@gmail.com>
2022-02-24 11:36:04 -0600

    add bfs 5 solution

--- week-6/d4-graphs/practices/graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
+++ week-6/d4-graphs/practices/graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
@@ -1,14 +1,29 @@
 const adjList = {
-    1: [2, 5],
-    2: [1, 3, 5],
-    3: [2, 4],
-    4: [3, 5],
-    5: [1, 2, 4],
-    6: []
-}
+  1: [2, 5],
+  2: [1, 3, 5],
+  3: [2, 4],
+  4: [3, 5],
+  5: [1, 2, 4],
+  6: [],
+};
 
 function aShortestPath(start, end) {
   // your code here
+  let queue = [[start]];
+  let visited = new Set().add(start);
+
+  while (queue.length > 0) {
+    let currentPath = queue.shift();
+    const currentNode = currentPath[currentPath.length - 1];
+    if (currentNode === end) return currentPath;
+    adjList[currentNode].forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        queue.push(currentPath.concat(neighbor));
+      }
+    });
+  }
+  return false;
 }
 
 console.log("First Test:");
