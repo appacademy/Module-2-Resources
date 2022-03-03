ca52933855deacf25f3041bef4326e95556f69ca
alex pezzati <aapezzati@gmail.com>
2022-03-03 16:56:07 -0500

    week 6 reset

--- week-6/d4-graphs/practices/social-graph-practice/social.js
+++ week-6/d4-graphs/practices/social-graph-practice/social.js
@@ -1,5 +1,6 @@
 // Implement the SocialNetwork class here
 class SocialNetwork {
+
   constructor() {
     this.users = {};
     this.follows = {};
@@ -8,63 +9,26 @@ class SocialNetwork {
 
   addUser(name) {
     // Your code here
-    this.currentID++;
-    const newId = this.currentID;
-    this.users[newId] = { id: newId, name: name };
-    this.follows[newId] = new Set();
-    return newId;
   }
 
   getUser(userID) {
     // Your code here
-    if (this.users[userID]) return this.users[userID];
-    else return null;
   }
 
   follow(userID1, userID2) {
     // Your code here
-    if (!this.users[userID1] || !this.users[userID2]) return false;
-    this.follows[userID1].add(userID2);
-    return true;
   }
 
   getFollows(userID) {
     // Your code here
-    return this.follows[userID];
   }
 
   getFollowers(userID) {
     // Your code here
-    const followers = new Set();
-    for (let id in this.follows) {
-      if (this.follows[id].has(userID)) followers.add(parseInt(id));
-    }
-    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
     // Your code here
-    const queue = [[userID]];
-    const visited = new Set().add(userID);
-
-    const recommended = [];
-
-    while (queue.length) {
-      console.log({ queue });
-      const currentPath = queue.shift();
-      const currentID = currentPath[currentPath.length - 1];
-      if (currentPath.length > degrees + 2) break;
-      if (currentPath.length > 2) recommended.push(currentID);
-
-      for (let follow of this.follows[currentID]) {
-        if (!visited.has(follow)) {
-          visited.add(follow);
-          queue.push(currentPath.concat(follow));
-        }
-      }
-    }
-    console.log('recommended', recommended)
-    return recommended;
   }
 }
 
