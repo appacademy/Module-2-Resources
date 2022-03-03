d082aa0515aed39f14ab3bf996e2745affc46a54
alex pezzati <aapezzati@gmail.com>
2022-03-03 16:54:55 -0500

    week 5 reset

--- week-5/d2-Logic-Memory-Arrays/practices/dynamic-array/dynamic-array.js
+++ week-5/d2-Logic-Memory-Arrays/practices/dynamic-array/dynamic-array.js
@@ -3,28 +3,19 @@ class DynamicArray {
     constructor(defaultSize=4) {
 
       // Fill this out
-      this.data = new Array(defaultSize);
-      this.capacity = defaultSize;
-      this.size = 0;
+
     }
 
     read(index) {
 
       // Fill this out
       // You may not use any built-in JS array functions
-      if (index >= this.size) return undefined;
-      return this.data[index];
     }
 
     unshift(val) {
 
       // Fill this out
       // You may not use any built-in JS array functions
-      for (let i = this.size; i > 0; i--){
-        this.data[i] = this.data[i - 1];
-      }
-      this.data[0] = val;
-      this.size++;
 
     }
 
--- week-5/d3-linked-lists-and-queues/practices/linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/linked-lists/doubly-linked-list.js
@@ -9,44 +9,14 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
-    //O(1)
-    const newNode = new DoublyLinkedListNode(val);
-
-    //non empty list
-    if (this.length >= 1) {
-      this.head.prev = newNode;
-      newNode.next = this.head;
-      this.head = newNode;
-    } else {
-      //empty list
-      this.head = newNode;
-      this.tail = newNode;
-    }
-    this.length++;
   }
 
   addToTail(val) {
     // Your code here
-    //O(1)
-    const newNode = new DoublyLinkedListNode(val);
-
-    //not empty list
-    if (this.length >= 1) {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
-    } else { //empty list
-      this.head = newNode;
-      this.tail = newNode;
-    }
-    this.length++;
   }
 
   // You can use this function to help debug
@@ -63,14 +33,3 @@ class DoublyLinkedList {
 }
 
 module.exports = DoublyLinkedList;
-
-const list = new DoublyLinkedList()
-// const node1 = new DoublyLinkedListNode('A')
-// const node2 = new DoublyLinkedListNode("B");
-// const node3 = new DoublyLinkedListNode("C");
-list.addToHead('A')
-list.addToTail('B')
-list.addToTail('C')
-
-list.print()
-
