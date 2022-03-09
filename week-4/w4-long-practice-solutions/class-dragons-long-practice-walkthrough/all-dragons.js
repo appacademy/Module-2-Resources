713cee4489494fa84e90f68cdc456654a3066905
alex pezzati <aapezzati@gmail.com>
2022-03-08 21:10:33 -0500

    walkthrough complete

--- week-4/w4-long-practice-solutions/class-dragons-long-practice-walkthrough/all-dragons.js
+++ week-4/w4-long-practice-solutions/class-dragons-long-practice-walkthrough/all-dragons.js
@@ -1,4 +1,32 @@
-// Your code here
+const FriendlyDragon = require('./classes/friendly-dragon')
+const EvilDragon = require('./classes/evil-dragon')
+const Dragon = require('./classes/dragon')
+
+const falkor = new FriendlyDragon(
+  'Falkor',
+  'white',
+  [
+    "save Atreyu from the swamp",
+    "save Atreyu from the Nothing",
+    "scare the local bullies into a dumpster"
+  ],
+  'Bastian'
+)
+
+const smaug = new EvilDragon(
+  "Smaug",
+  "black",
+  [
+    "take over your mountain kingdom",
+    "steal all your dwarven gold",
+    "burn down your floating village",
+  ],
+  "Dwarf King"
+)
+
+const allDragons = Dragon.getDragons(falkor, smaug)
+
+console.log(allDragons)
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
@@ -7,6 +35,7 @@ try {
   module.exports = {
     falkor,
     smaug,
+    allDragons
   };
 } catch {
   module.exports = null;
--- week-4/w4-long-practice-solutions/class-dragons-long-practice-walkthrough/classes/dragon.js
+++ week-4/w4-long-practice-solutions/class-dragons-long-practice-walkthrough/classes/dragon.js
@@ -1,4 +1,17 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    return dragons.map(dragon => dragon.name)
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
