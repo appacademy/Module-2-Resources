53565d47aee6ac160ba8a71b363f25afa7e4b900
Kristen Chauncey <chaunceykristen@gmail.com>
2022-03-09 11:27:36 -0600

    add bind solution

--- week-4/d3-context-bind-call-apply/practices/01-context-regular-functions/context.js
+++ week-4/d3-context-bind-call-apply/practices/01-context-regular-functions/context.js
@@ -13,7 +13,6 @@ class Fish {
 }
 
 const nemo = new Fish('Nemo');
-
 /********************************* Scenario 1 *********************************/
 // eat(); // ?
 
@@ -33,5 +32,9 @@ const nemo = new Fish('Nemo');
 
 
 /********************************* Scenario 5 *********************************/
-// const swim = nemo.swim;
-// swim(); // ?
+const swim = nemo.swim.bind(nemo);
+swim(); // ?
+const dory = new Fish('dory')
+dory.swim()
+const swim2 = dory.swim
+swim2()
