e5fd91b3b4418fecdcb641ae184b53e8827b49b7
jeff <jlgranof@icloud.com>
2022-04-05 15:08:43 -0500

    poly guided practice

--- week-4/d2-es6-classes/practices/04-polymorphism/classes/student.js
+++ week-4/d2-es6-classes/practices/04-polymorphism/classes/student.js
@@ -16,6 +16,10 @@ class Student extends Person {
       return "Both students have the same GPA";
     }
   }
+
+  introduce() {
+    return `Hello. I am ${this.firstName} ${this.lastName}, and I study ${this.major}.`;
+  }
 }
 
 /****************************************************************************/
