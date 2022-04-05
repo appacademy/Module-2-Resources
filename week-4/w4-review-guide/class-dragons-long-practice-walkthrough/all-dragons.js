6a119e943cb1cfe8761a213266f82fceb32d741e
alex pezzati <aapezzati@gmail.com>
2022-04-05 18:50:58 -0400

    dragons long practice setup

---
+++ week-4/w4-review-guide/class-dragons-long-practice-walkthrough/all-dragons.js
@@ -0,0 +1,13 @@
+// Your code here
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = {
+    falkor,
+    smaug,
+  };
+} catch {
+  module.exports = null;
+}
---
+++ week-4/w4-review-guide/class-dragons-long-practice-walkthrough/classes/dragon.js
@@ -0,0 +1,10 @@
+// Your code here
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Dragon;
+} catch {
+  module.exports = null;
+}
