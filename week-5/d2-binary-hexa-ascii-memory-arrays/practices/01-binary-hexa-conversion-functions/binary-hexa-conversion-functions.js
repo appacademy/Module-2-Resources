19d13a2fc7049d1978df4496ddd7be4068ebb1e4
alex pezzati <aapezzati@gmail.com>
2022-04-12 12:09:01 -0400

    binary hexa conversion

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/01-binary-hexa-conversion-functions/binary-hexa-conversion-functions.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/01-binary-hexa-conversion-functions/binary-hexa-conversion-functions.js
@@ -9,48 +9,49 @@ Consult documentation on how to utilize the following methods:
 Base 10 to binary
 */
 function deciToBinary(blob) {
-    // Your code here
+    return `0b${blob.toString(2)}`
 }
 
-// console.log('Base 10 to binary:')
-// console.log(deciToBinary(13))  // '0b1101'
-// console.log(deciToBinary(72))  // '0b1001000'
-// console.log('')
+console.log('Base 10 to binary:')
+console.log(deciToBinary(13))  // '0b1101'
+console.log(deciToBinary(72))  // '0b1001000'
+console.log('')
 
 /*
 Base 10 to hexadecimal
 */
 function deciToHexa(blob) {
-    // Your code here
+    return `0x${blob.toString(16)}`
 }
 
-// console.log('Base 10 to hexadecimal:')
-// console.log(deciToHexa(9))   // '0x9'
-// console.log(deciToHexa(32))  // '0x20'
-// console.log('')
+console.log('Base 10 to hexadecimal:')
+console.log(deciToHexa(9))   // '0x9'
+console.log(deciToHexa(32))  // '0x20'
+console.log('')
 
 /*
 Binary to base 10
 */
 function binaryToDeci(blob) {
-    // Your code here
+    return parseInt(blob.substring(2), 2)
 }
 
-// console.log('Binary to base 10:')
-// console.log(binaryToDeci('0b0101'))      // 5
-// console.log(binaryToDeci('0b10001000'))  // 136
-// console.log('')
+console.log('Binary to base 10:')
+console.log(binaryToDeci('0b0101'))      // 5
+console.log(binaryToDeci('0b10001000'))  // 136
+console.log('')
 
 /*
 Hexadecimal to base 10
 */
 function hexaToDeci (blob) {
-    // Your code here
+    return parseInt(blob, 16)
+    // return parseInt(blob.substring(2), 16)
 }
 
-// console.log('Hexadecimal to base 10:')
-// console.log(hexaToDeci('0x51'))    // 81
-// console.log(hexaToDeci('0x10ff'))  // 4351
+console.log('Hexadecimal to base 10:')
+console.log(hexaToDeci('0x51'))    // 81
+console.log(hexaToDeci('0x10ff'))  // 4351
 
 
 module.exports = {
