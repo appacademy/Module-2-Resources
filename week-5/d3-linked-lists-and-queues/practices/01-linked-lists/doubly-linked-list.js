9f6117092568a6f8471a3fb88df03641d4fac66e
jeff <jlgranof@icloud.com>
2022-04-13 13:09:00 -0500

    done with lecture

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
@@ -9,14 +9,50 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
+  // Step 1: create the new node
+  // Step 2: add the node to the list
+  // Step 3: incrementing the length of the list
   addToHead(val) {
     // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+
+    // Case 1: No Head
+    if (!this.head) {
+      this.head = newNode;
+      this.tail =  newNode;
+    } else { // Case 2: There is a Head
+      // Changing the old heads previous pointer to the new Node
+      this.head.prev = newNode;
+
+      // Assign new Nodes next property to be the current head
+      newNode.next = this.head;
+
+      // Change head pointer to the new Node, making it the new head
+      this.head = newNode;
+    }
+
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail =  newNode;
+    }
+
+    this.length++;
   }
 
   // You can use this function to help debug
