524b4c583945cfe40c0617ce629459a227807d1a
jeff <jlgranof@icloud.com>
2022-04-20 11:48:59 -0500

    phase 2

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -11,14 +11,36 @@ class BinarySearchTree {
 
   constructor() {
     // Your code here
+    this.root = null;
   }
-
+  
   insert(val, currentNode=this.root) {
-    // Your code here
+    // We have no root, insert into the root
+    if (!this.root) {
+      this.root = new TreeNode(val);
+      return;
+    }
+
+    if (val < currentNode.val) {
+      if (!currentNode.left) currentNode.left = new TreeNode(val);
+      else this.insert(val, currentNode.left);
+    } else {
+      if (!currentNode.right) currentNode.right = new TreeNode(val);
+      else this.insert(val, currentNode.right);
+    }
   }
 
   search(val) {
     // Your code here
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (val < currentNode.val) currentNode = currentNode.left;
+      else if (val > currentNode.val) currentNode = currentNode.right;
+      else return true;
+    }
+
+    return false;
   }
 
 
