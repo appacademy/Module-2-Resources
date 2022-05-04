33ebbaebd50f2b074b927e5d80d683e5715281a4
jeff <jlgranof@icloud.com>
2022-05-04 12:08:28 -0500

    call/apply

--- week-4/d3-context-bind-call-apply/practices/03-call-apply/greet.js
+++ week-4/d3-context-bind-call-apply/practices/03-call-apply/greet.js
@@ -6,5 +6,12 @@ function say(name) {
 helloMessage = { message: 'Hello,' };
 heyThereMessage = { message: 'Hey there,' };
 
-say(helloMessage, "John");
-say(heyThereMessage, "Michael");
+say.call(helloMessage, "John");
+say.call(heyThereMessage, "Michael");
+
+// {
+//   message: 'hello',
+//   function say (name) {
+//     console.log(this.message)
+//   }
+// }
