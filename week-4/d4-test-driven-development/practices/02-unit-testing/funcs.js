f6fd714880627e36e5df56f9e43fdb0ebf1b0d77
jeff <jlgranof@icloud.com>
2022-05-09 14:02:51 -0500

    idk

--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,9 +1,13 @@
 function isFive(num) {
   // Your code here
+  return num === 5;
 }
 
 function isOdd(number) {
   // Your code here
+  if (typeof number !== 'number') throw Error("NOT A NUMBER");
+
+  return number % 2 !== 0;
 }
 
 function myRange(min, max, step = 1) {
