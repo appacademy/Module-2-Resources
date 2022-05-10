3315858d7b6b711892036db11e8d5e0c23392d73
alex pezzati <aapezzati@gmail.com>
2022-05-10 13:21:10 -0400

    ascii advanced conversion functions and memory quiz

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/02-ascii-advanced-conversion-functions/ascii-advanced-conversion-functions.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/02-ascii-advanced-conversion-functions/ascii-advanced-conversion-functions.js
@@ -17,27 +17,41 @@ function binaryToHexadecimal(blob) {
 
 /* Base 16 to base 2 */
 function hexadecimalToBinary(blob) {
-  // Your code here
+  const decimal = parseInt(blob, 16)
+  return `0b${decimal.toString(2)}`
 }
 
 /* Base 10 to ASCII */
 function decimalToAscii(blob) {
-  // Your code here
+  return String.fromCharCode(blob)
 }
 
 /* Base 2 to ASCII */
 function binaryToAscii(blob) {
-  // Your code here
+  // const decimal = parseInt(blob.substring(2), 2)
+  // return String.fromCharCode(decimal)
+  return String.fromCharCode(blob)
 }
 
 /* Base 16 to ASCII */
 function hexadecimalToAscii(blob) {
-  // Your code here
+  return String.fromCharCode(blob)
 }
 
 /* ASCII to base 10 */
 function asciiToDecimal(blob) {
-  // Your code here
+  const test = 'A'
+  console.log(test.charCodeAt(0))
+  console.log(test.charCodeAt())
+
+  const charCodes = []
+
+  for (let i = 0; i < blob.length; i++) {
+    charCodes.push(blob[i].charCodeAt())
+    // charCodes.push(blob.charCodeAt(i))
+  }
+
+  return charCodes
 }
 
 // console.log('Binary to hexadecimal:')
@@ -65,9 +79,9 @@ function asciiToDecimal(blob) {
 // console.log(hexadecimalToAscii('0x63'))  // 'c'
 // console.log('')
 
-// console.log('ASCII to decimal:')
-// console.log(asciiToDecimal('HELLO'))   // [ 72, 69, 76, 76, 79 ]
-// console.log(asciiToDecimal('world!'))  // [ 119, 111, 114, 108, 100, 33 ]
+console.log('ASCII to decimal:')
+console.log(asciiToDecimal('HELLO'))   // [ 72, 69, 76, 76, 79 ]
+console.log(asciiToDecimal('world!'))  // [ 119, 111, 114, 108, 100, 33 ]
 
 module.exports = {
   binaryToHexadecimal,
