7070b50f5bdc07da076760bb1edcbc2c2ca47b88
jeff <jlgranof@icloud.com>
2022-05-12 12:24:41 -0500

    pt 3 finished

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -12,22 +12,55 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets).fill(null);
   }
 
   hash(key) {
-    // Your code here
+    // Get the first 8 characters of the sha256 hash
+    const hashKey = sha256(key).slice(0,8);
+    
+    // convert to base 10 integer and return
+    return parseInt(`0x${hashKey}`);
   }
 
   hashMod(key) {
-    // Your code here
+    // get the index after hashing and return
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    // find the bucket index 
+    const index = this.hashMod(key);
+    
+    // Inserting or throwing an error
+    if (this.data[index]) {
+      throw Error('hash collision or same key/value pair already exists!')
+    } else {
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    }
   }
 
   insertWithHashCollisions(key, value) {
     // Your code here
+    const index = this.hashMod(key);
+    const newPair = new KeyValuePair(key, value);
+
+    if (this.data[index]) {
+      // deal with the hash collision
+      newPair.next = this.data[index];
+      this.data[index] = newPair;
+    } else {
+      this.data[index] = newPair;
+    }
+
+    // shorter way to write this
+    // if (this.data[index]) newPair.next = this.data[index]
+    // this.data[index] = newPair
+
+    this.count++;
   }
 
   insert(key, value) {
