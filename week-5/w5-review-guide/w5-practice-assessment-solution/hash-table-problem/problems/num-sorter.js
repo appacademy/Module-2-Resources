277b3011fbfa727696a37a2757ec76975d79f669
alex pezzati <aapezzati@gmail.com>
2022-05-13 19:34:29 -0400

    w5 pa solutions

--- week-5/w5-review-guide/w5-practice-assessment-solution/hash-table-problem/problems/num-sorter.js
+++ week-5/w5-review-guide/w5-practice-assessment-solution/hash-table-problem/problems/num-sorter.js
@@ -61,9 +61,9 @@ Below is the NumSorter class that is to be refactored
 */
 class NumSorter {
   constructor() {
-    this.numList = [];
+    this.numList = new LinkedList(); // queue??
 
-    this.allowedNums = [];
+    this.allowedNums = new Set(); // set??
   }
 
   /*
@@ -71,8 +71,8 @@ class NumSorter {
   Should not have any duplicates in allowedNums
   */
   addAllowedNum(num) {
-    if (!this.allowedNums.includes(num)) {
-    this.allowedNums.push(num);
+    if (!this.allowedNums.has(num)) {  // array.includes() O(n) set.has() O(1)
+    this.allowedNums.add(num);
     return `${num} added to allowedNums`;
     } else {
     return `${num} already in allowedNums`;
@@ -81,7 +81,7 @@ class NumSorter {
 
   /* Returns true if the number is allowed, false otherwise */
   isNumAllowed(num) {
-    return this.allowedNums.includes(num);
+    return this.allowedNums.has(num);
   }
 
   /*
@@ -89,8 +89,8 @@ class NumSorter {
   Returns value at the back of numList
   */
   addNumToBack(num) {
-    if (this.isNumAllowed(num)) this.numList.push(num);
-    return this.numList[this.numList.length - 1];
+    if (this.isNumAllowed(num)) this.numList.enqueue(num);
+    return this.numList.tail.value;
   }
 
   /*
@@ -99,7 +99,7 @@ class NumSorter {
   */
   getFirstNum() {
     if(this.numList.length > 0){
-    return this.numList.shift();
+    return this.numList.dequeue(); // array.shift() O(n) ll.enqueue() O(1)
     } else {
     return undefined;
     }
@@ -107,12 +107,13 @@ class NumSorter {
 
   /* Returns the count of nums in numList */
   numCount() {
-    let count = 0;
-    while (this.numList[count] !== undefined) {
-    count++;
-    }
-    
-    return count;
+    // let count = 0;
+    // while (this.numList[count] !== undefined) {  // O(n) queue.length O(1)
+    // count++;
+    // }
+
+    // return count;
+    return this.numList.length
   }
 
   /*
@@ -120,11 +121,11 @@ class NumSorter {
   Only include allowed numbers; returns amount of nums in numList
   */
   buildNumList(amount) {
-    this.numList = [];
+    this.numList = new LinkedList();
 
     for (let i = 0; i <= amount; i++) {
-    if (this.allowedNums.includes(i)) {
-    this.numList.push(i);
+    if (this.allowedNums.has(i)) {
+    this.numList.enqueue(i);
     }
     }
 
