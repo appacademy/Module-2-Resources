c87fee6b23c6eda2fd563b69585f127a242f373c
jeff <jlgranof@icloud.com>
2022-05-19 12:57:20 -0500

    deg of sep

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/06-degress-of-separation-bfs.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/06-degress-of-separation-bfs.js
@@ -9,6 +9,27 @@ const adjList = {
 
 function degreesOfSeparation(start, end) {
   // your code here
+  let queue = [[start]];
+  let visited = new Set();
+  visited.add(start);
+
+  while (queue.length) {
+    let currentPath = queue.shift();
+    let currentNode = currentPath[currentPath.length - 1];
+
+    // Comparison here
+    if (currentNode === end) return currentPath.length - 1;
+
+    let neighbors = adjList[currentNode];
+    neighbors.forEach(neighbor => {
+      if (!visited.has(neighbor)) {
+        queue.push([...currentPath, neighbor]);
+        visited.add(neighbor);
+      }
+    });
+  }
+
+  return false;
 }
 
 console.log("First Test:");
