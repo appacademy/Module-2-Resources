959d0351e4be041e49ee7d1f553f9bb1a7bfc967
alex pezzati <aapezzati@gmail.com>
2022-06-09 12:16:27 -0400

    hash table practice pt1

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -1,40 +1,44 @@
 const sha256 = require('js-sha256');
 
 class KeyValuePair {
-  constructor(key, value) {
-    this.key = key;
-    this.value = value;
-    this.next = null;
-  }
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class HashTable {
+    constructor(numBuckets = 4) {
+        this.count = 0;
+        this.capacity = numBuckets;
+        this.data = new Array(this.capacity).fill(null);
+    }
 
-  constructor(numBuckets = 4) {
-    // Your code here
-  }
+    hash(key) {
+        const hashHexString = sha256(key).slice(0, 8);
+        return parseInt(`0x${hashHexString}`);
+        // return parseInt(hashHexString, 16);
+    }
 
-  hash(key) {
-    // Your code here
-  }
+    // obj.keyname = value
+    hashMod(key) {
+        // console.log(this.hash(key) % this.capacity);
+        return this.hash(key) % this.capacity;
+    }
 
-  hashMod(key) {
-    // Your code here
-  }
+    insertNoCollisions(key, value) {
+        // Your code here
+        // const index = hashmodoutputhere
+    }
 
-  insertNoCollisions(key, value) {
-    // Your code here
-  }
+    insertWithHashCollisions(key, value) {
+        // Your code here
+    }
 
-  insertWithHashCollisions(key, value) {
-    // Your code here
-  }
-
-  insert(key, value) {
-    // Your code here
-  }
-
+    insert(key, value) {
+        // Your code here
+    }
 }
-
 
 module.exports = HashTable;
