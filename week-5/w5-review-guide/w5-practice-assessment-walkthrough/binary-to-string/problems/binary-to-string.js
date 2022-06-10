bda2e301d01169435b2eb26f85d636d40b2d9968
alex pezzati <aapezzati@gmail.com>
2022-06-10 17:51:21 -0400

    w5 p assessment walkthrough setup

---
+++ week-5/w5-review-guide/w5-practice-assessment-walkthrough/binary-to-string/problems/binary-to-string.js
@@ -0,0 +1,10 @@
+function binaryToString(binaryBlob) {
+  // Your code here
+}
+
+/* Comment in the code below to run local tests */
+// console.log(binaryToString('010000010100001001000011')); // 'ABC'
+// console.log(binaryToString('001101100011011100111000')); // '678'
+
+
+module.exports = binaryToString;
