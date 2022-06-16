6f4aba5b36f0a11f6dfd4530eb06b549292eb343
alex pezzati <aapezzati@gmail.com>
2022-06-15 20:21:42 -0400

    walkthrough complete

--- week-6/w6-long-practice-solutions/d3/binary-tree-long-practice-walkthrough/tree-practice.js
+++ week-6/w6-long-practice-solutions/d3/binary-tree-long-practice-walkthrough/tree-practice.js
@@ -50,7 +50,7 @@ function findMaxBT (rootNode) {
 
 function getHeight (rootNode) {
   if (!rootNode) return 0;
-  
+
   let height = -1;
   const q = [rootNode];
   while (q.length) {
@@ -60,27 +60,106 @@ function getHeight (rootNode) {
       if (curr.left) q.push(curr.left);
       if (curr.right) q.push(curr.right);
     }
-    
+
     height++;
   }
-  
+
   return height;
 }
 
 function countNodes (rootNode) {
-  // Your code here
+  const queue = [rootNode]
+
+  let count = 0
+
+  while (queue.length) {
+    const current = queue.shift()
+
+    count++
+
+    if (current.left) queue.push(current.left)
+    if (current.right) queue.push(current.right)
+  }
+
+  return count
 }
 
 function balancedTree (rootNode) {
-  // Your code here
+  return Math.log2(countNodes(rootNode)) >= getHeight(rootNode)
 }
 
 function getParentNode (rootNode, target) {
-  // Your code here
+  if (rootNode.val === target) return null
+
+  const stack = [rootNode]
+
+  while (stack.length) {
+    const current = stack.pop()
+
+    if (current.left && current.left.val === target ||
+       current.right && current.right.val === target) {
+        return current
+      }
+
+    if (current.left) stack.push(current.left)
+    if (current.right) stack.push(current.right)
+  }
+
+  return undefined
 }
 
 function inOrderPredecessor (rootNode, target) {
-  // Your code here
+  // let prev = null
+  // let found = false
+
+  // function _dfs (root) {
+  //   if (!root) return
+
+  //   _dfs(root.left)
+
+  //   if (root.val === target) {
+  //     found = true
+  //     return
+  //   }
+  //   if (!found) prev = root.val
+
+  //   _dfs(root.right)
+  // }
+
+  // // (function (root) {
+  // //   if (!root) return
+
+  // //   _dfs(root.left)
+
+  // //   if (root.val === target) {
+  // //     found = true
+  // //     return
+  // //   }
+  // //   if (!found) prev = root.val
+
+  // //   _dfs(root.right)
+  // // })()
+
+  // _dfs(rootNode)
+  // return prev
+
+  let current = rootNode;
+  let stack = [];
+  let prev = null;
+
+  while (stack.length || current) {
+      if (current) {
+          stack.push(current);
+          current = current.left;
+      } else {
+          current = stack.pop();
+          if (current.val === target) {
+              return prev;
+          }
+          prev = current.val;
+          current = current.right;
+      }
+  }
 }
 
 
