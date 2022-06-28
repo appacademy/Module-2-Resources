992248ab2f44c1b14ca5bd86d1445babebea2933
Kristen <chaunceykristen@gmail.com>
2022-06-28 12:08:26 -0500

    add cjs solution

--- week-4/d2-es6-classes/practices/01-common-js-modules/messages/say-message.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/messages/say-message.js
@@ -1,7 +1,11 @@
-
+const helloMessage = require('./hello-message')
+// const { helloMessage } = require('./hello-message')
 
 function sayMessage(message) {
   console.log(`"${message}"`)
 }
 
 sayMessage(helloMessage);
+
+module.exports = sayMessage
+// module.exports.sayMessage = sayMessage
