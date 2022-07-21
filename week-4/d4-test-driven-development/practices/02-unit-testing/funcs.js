7a2adae86e939b95a8e0bc9bb46668198737624f
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-21 18:42:30 -0500

    unit-testing

--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,13 +1,19 @@
 function isFive(num) {
-  // Your code here
+  return num === 5 ? true : false;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (!(typeof number === 'number')) throw new Error();
+
+  return (number % 2 === 0) ? false : true;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  const arr = [];
+  for ( let i = min; i <= max; i += step) {
+    arr.push(i)
+  }
+  return arr;
 }
 
 
--- week-4/d4-test-driven-development/practices/02-unit-testing/test/class-spec.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/test/class-spec.js
@@ -4,31 +4,37 @@ const expect = chai.expect;
 const { Word } = require("../class");
 
 describe("Word", function () {
+
+  let word;
+
+  beforeEach(() => {
+    word = new Word('word')
+  })
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word).to.haveOwnProperty('word')
     });
 
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.eql('word');
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeVowels()).to.eql('wrd')
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(word.removeConsonants()).to.eql('o');
     });
   });
 
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(word.pigLatin()).to.eql('ordway')
     });
   });
 });
