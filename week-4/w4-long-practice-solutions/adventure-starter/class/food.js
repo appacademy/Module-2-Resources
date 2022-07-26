2870824540443d2c3f126622b0d31e2b4854fa62
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-26 18:56:49 -0500

    W4D2 Long Practice solutions

---
+++ week-4/w4-long-practice-solutions/adventure-starter/class/food.js
@@ -0,0 +1,12 @@
+const { Item } = require('./item');
+// FILL THIS OUT
+
+class Food extends Item {
+  constructor(name, description) {
+    super(name, description);
+  }
+} 
+
+module.exports = {
+  Food,
+};
---
+++ week-4/w4-long-practice-solutions/adventure-starter/class/item.js
@@ -0,0 +1,11 @@
+class Item {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
+
+}
+
+module.exports = {
+  Item,
+};
