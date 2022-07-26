a6af87582245b6f425ec8b6a2639605e52fcf8ed
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-26 12:44:27 -0500

    Inheritance solution

--- week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
@@ -1,7 +1,23 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
 
+    console.log(this)
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let sum = 0;
+    teachers.forEach(teacher => {
+      sum += teacher.yearsOfExperience;
+    });
+    return sum;
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
