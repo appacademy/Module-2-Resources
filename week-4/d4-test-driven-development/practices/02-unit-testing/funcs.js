8badb8ee599a2f0f214fabba56d4f6a6348c190f
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-28 18:30:15 -0500

    w4d4 long practice solution

--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,13 +1,36 @@
 function isFive(num) {
   // Your code here
+  return num === 5;
+  
+  // if (num === 5) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 function isOdd(number) {
-  // Your code here
+
+  if (typeof number !== 'number') throw new Error();
+
+  return number % 2 !== 0;
+
+
+  // if (number % 2 === 0) {
+  //   return false;
+  // } else if (typeof number !== 'number') {
+  //   throw new Error()
+  // }
+  // return true;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let newArray = [];
+
+    for (let i = min; i <= max; i += step) {
+      newArray.push(i);
+    }
+  return newArray;
 }
 
 
