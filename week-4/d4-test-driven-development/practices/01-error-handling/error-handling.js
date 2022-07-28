dfce862ef221991fc4624426df965380344b319a
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-07-28 11:43:23 -0500

    error-handling

--- week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
+++ week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
@@ -1,8 +1,14 @@
-// 1.
+1.
 function sum(array) {
   let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+  try {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  } catch(e) {
+    if (e instanceof TypeError) {
+      console.error(e.message);
+    } else throw e
   }
   return sum;
 }
@@ -12,11 +18,24 @@ console.log(res);
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+
+function sayName(name) {
+  if (typeof name === "string") {
+    console.log(name);
+  } else {
+    throw new TypeError("Invalid name! Must be a string!")
+  }
+}
+
+try {
+  sayName("Alex");
+  sayName(1);
+} catch(e) {
+  console.error(e.message);
+}
 // Your code here
 
-// 3.
+// // 3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
@@ -24,3 +43,15 @@ function greet(greeting) {
 
   console.log(greeting);
 }
+
+try {
+  greet(1)
+  greet('Howdy')
+  greet();
+  greet('Hey there')
+
+} catch(e) {
+  console.log('Hello World')
+}
+
+
