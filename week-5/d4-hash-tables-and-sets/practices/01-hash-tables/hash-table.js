f48ced581c152dce46d34dacd664ed6a767fef68
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-08-04 12:21:09 -0500

    hash table 3

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -46,6 +46,17 @@ class HashTable {
 
   insertWithHashCollisions(key, value) {
     // Your code here
+    const kvp = new KeyValuePair(key, value);
+    const idx = this.hashMod(key);
+
+    if (!this.data[idx]) {
+      this.data[idx] = kvp
+    } else {
+      let oldKey = this.data[idx];
+      kvp.next = oldKey;
+      this.data[idx] = kvp;
+    }
+    this.count++;
   }
 
   insert(key, value) {
