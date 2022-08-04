ec6b11545549bc05a361ba311c66a918b1cb840f
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-08-04 11:41:48 -0500

    hash table 1 and 2

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -12,18 +12,36 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
     // Your code here
+    const hashKey = sha256(key).substring(0,8);
+
+    return parseInt(`0x${hashKey}`);
+    // return parseInt(`0x${sha256(key).substring(0,8)}`);
   }
 
   hashMod(key) {
     // Your code here
+    return this.hash(key) % this.capacity;
+
   }
 
   insertNoCollisions(key, value) {
     // Your code here
+    const kvp = new KeyValuePair(key, value);
+    const idx = this.hashMod(key);
+
+    if (this.data[idx]) {
+      throw Error('hash collision or same key/value pair already exists!')
+    } else {
+      this.data[idx] = kvp;
+      this.count++;
+    }
   }
 
   insertWithHashCollisions(key, value) {
