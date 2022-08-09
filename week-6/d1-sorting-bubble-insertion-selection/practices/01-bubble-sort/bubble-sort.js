186a506577a57abfc822274e257d3831c7e9e111
yinyang117 <slcryan2010@yahoo.com>
2022-08-08 18:02:58 -0600

    Monday

--- week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
@@ -1,17 +1,33 @@
 
 function bubbleSort(arr) {
 
-  // Iterate through the array
+  let hasSwapped = true;
 
-    // If the current value is greater than its neighbor to the right
-      // Swap those values
+  while (hasSwapped) {
+    hasSwapped = false;
 
-      // Do not move this console.log
-      console.log(arr.join(","));
+    // Iterate through the array
+    for (let i = 0; i < arr.length - 1; i++) {
+      // If the current value is greater than its neighbor to the right
+      if (arr[i] > arr[i + 1]) {
+        // Swap those values
+        // let temp = arr[i]
+        // arr[i] = arr[i + 1]
+        // arr[i + 1] = temp
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]; //Does the same as the 3 lines above
+        hasSwapped = true;
 
-  // If you get to the end of the array and no swaps have occurred, return
-
-  // Otherwise, repeat from the beginning
+        // Do not move this console.log
+        console.log(arr.join(","));
+      }
+    }
+  
+    // If you get to the end of the array and no swaps have occurred, return
+    // if (!hasSwapped) return arr;
+  
+    // Otherwise, repeat from the beginning
+  }
+  return arr;
 
 }
 
