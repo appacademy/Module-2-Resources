9d84f747ec665df25506f0ec537d4b11ceb0f722
yinyang117 <slcryan2010@yahoo.com>
2022-08-11 17:22:29 -0600

    social practice

--- week-6/d4-graphs/practices/02-social-graph-practice/social.js
+++ week-6/d4-graphs/practices/02-social-graph-practice/social.js
@@ -8,27 +8,71 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID++;
+    const nextID = this.currentID;
+
+    this.users[nextID] = {id: nextID, name: name};
+    this.follows[nextID] = new Set();
+
+    return nextID;
   }
 
   getUser(userID) {
-    // Your code here
+    if (this.users[userID]) return this.users[userID];
+    else return null;
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    if (!this.users[userID1] || !this.users[userID2]) {
+      return false;
+    }
+
+    this.follows[userID1].add(userID2);
+    return true;
   }
 
   getFollows(userID) {
-    // Your code here
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here
+    const followers = new Set();
+
+    for (let id in this.follows) {
+      if (this.follows[id].has(userID)) followers.add(parseInt(id));
+    }
+
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
-    // Your code here
+    let queue = [[userID]];
+
+    let recommended = [];
+
+    let visited = new Set();
+    visited.add(userID);
+
+    while (queue.length > 0) {
+
+      let path = queue.shift();
+      let id = path[path.length - 1];
+
+      if (path.length > degrees + 2) break;
+
+      if (path.length > 2) recommended.push(id);
+
+      for (let follow of this.follows[id]) {
+        if (!visited.has(follow)) {
+          visited.add(follow);
+          // Use concat to create a new array
+          // Or you could spread the array to create a copy
+          queue.push(path.concat([follow]))
+        }
+      }
+    }
+
+    return recommended;
   }
 }
 
