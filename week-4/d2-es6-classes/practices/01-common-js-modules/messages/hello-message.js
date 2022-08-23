b97b6d618c94d1f6acae92164773ab5d5d61c6e8
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-08-23 11:45:37 -0500

    commonJS modules solutions

--- week-4/d2-es6-classes/practices/01-common-js-modules/messages/hello-message.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/messages/hello-message.js
@@ -1,1 +1,13 @@
 const helloMessage = "Greetings! Let us begin!";
+// const goodbyeMessage = "Goodbye!"
+
+
+
+// exports.helloMessage = "Greetings! Let us begin!"
+// module.exports = { helloMessage, goodbyeMessage };
+// module.exports = {
+    //     helloMessage: helloMessage
+    // }
+
+    
+module.exports = {helloMessage};
