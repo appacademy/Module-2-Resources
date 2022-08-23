e64255271b08c81515fca41dbf3b264a9131b297
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-08-23 16:21:58 -0500

    added alternate solutions

--- week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
@@ -10,6 +10,14 @@ class Student extends Person {
 
   static compareGPA(stood1, stood2) {
 
+    // if (student1.GPA > student2.GPA) {
+    //   return `${student1.firstName} ${student1.lastName} has the higher GPA.`;
+    // } else if (student2.GPA > student1.GPA) {
+    //     return `${student2.firstName} ${student2.lastName} has the higher GPA.`;
+    // } else {
+    //     return 'Both students have the same GPA.';
+    // }
+
     // if (stood1.GPA === stood2.GPA) return "Both students have the same GPA."
     
     // return stood1.GPA > stood2.GPA ? `${stood1.firstName} ${stood1.lastName} has the higher GPA.` : `${stood2.firstName} ${stood2.lastName} has the higher GPA.`
