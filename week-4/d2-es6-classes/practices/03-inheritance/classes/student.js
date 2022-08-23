abd09b8f01efc85c2559c80e879201a618146eca
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-08-23 15:31:19 -0500

    inheritance lecture

--- week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
@@ -1,6 +1,23 @@
 const Person = require('./person');
 
 // Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(stood1, stood2) {
+
+    if (stood1.GPA === stood2.GPA) return "Both students have the same GPA."
+    
+    return stood1.GPA > stood2.GPA ? `${stood1.firstName} ${stood1.lastName} has the higher GPA.` : `${stood2.firstName} ${stood2.lastName} has the higher GPA.`
+  }
+}
+
+// const shane = new Student('shane', 'wilkey');
+// console.log(shane.age)
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
