9d3725dd23f391c65ea2fb16df40f9d667a4036f
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-08-29 19:05:40 -0500

    w5d1 lecture

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
@@ -3,21 +3,42 @@ const [addNums, addManyNums] = require("./phase-1");
 function addNums10Timing(increment) {
   // Copy your `addNums10` code here
   // Then, add timing code
+  const sums = [];
+
+  console.time(`addNums10Timing`);
+  for (let i = increment; i <= 50 * increment; i += increment) {
+    let start = Date.now();
+    sums.push(addNums(i));
+    let end = Date.now();
+    console.log(end - start);
+  }
+  console.timeEnd(`addNums10Timing`);
+  return sums;
 }
 
 
 function addManyNums10Timing(increment) {
 // Copy your `addManyNums10` code here
 // Then, add timing code
+  const sums = [];
+  console.time(`addManyNums10Timing`);
+  for (let i = increment; i <= 50 * increment; i += increment) {
+    let start = Date.now();
+    sums.push(addManyNums(i));
+    let end = Date.now();
+    console.log(end - start);
+  }
+  console.timeEnd(`addManyNums10Timing`);
+  return sums;
 }
 
 
 n = 1000
 console.log(`addNums(${n}): `);
-addNums10Timing(10000);
+addNums10Timing(10000000);
 
 console.log("\n***********\n");
 
 n = 1000
 console.log(`addManyNums(${n}): `);
-addManyNums10Timing(1000);
+addManyNums10Timing(10000);
