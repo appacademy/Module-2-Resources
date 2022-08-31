16f85bc44e07ffed2d78109089062daba52031f6
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-08-31 11:52:16 -0500

    linked list lecture 1

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -8,14 +8,37 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    this.length++;
+    const newNode = new LinkedListNode(val);
+    newNode.next = this.head;
+    this.head = newNode;
   }
 
   addToTail(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);
+    this.length++;
+    // Case 1: Linked list has no length
+    if (!this.head) {
+      this.head = newNode;
+      return this;
+    }
+
+    // Case 2: Linked list does have length
+    let current = this.head;
+
+    while (current.next!== null) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+    return this;
   }
 
   // You can use this function to help debug
