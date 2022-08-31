bb0533730e440b47f1c974993263aaed3d679653
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-08-31 11:53:07 -0500

    linked list lecture

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -5,40 +5,51 @@ class LinkedListNode {
   }
 }
 
+// For 1 liner
+// class LinkedListNode {
+//   constructor(val, next=null) {
+//     this.value = val;
+//     this.next = next;
+//   }
+// }
+
 class LinkedList {
   constructor() {
     // Your code here
     this.head = null;
-    this.length = 0;
+    this.length =  0;
   }
 
   addToHead(val) {
     // Your code here
-    this.length++;
     const newNode = new LinkedListNode(val);
+
     newNode.next = this.head;
     this.head = newNode;
+    // For 1 liner. Still need second line to increment length
+    // this.head = new LinkedListNode(val, this.head)
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
     const newNode = new LinkedListNode(val);
     this.length++;
-    // Case 1: Linked list has no length
+
+    // Case 1: There are no nodes in the list
     if (!this.head) {
       this.head = newNode;
       return this;
     }
 
-    // Case 2: Linked list does have length
-    let current = this.head;
+    // Case 2: The list does have nodes
+    let currentNode = this.head;
 
-    while (current.next!== null) {
-      current = current.next;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
     }
 
-    current.next = newNode;
-    return this;
+    currentNode.next = newNode;
   }
 
   // You can use this function to help debug
@@ -53,5 +64,11 @@ class LinkedList {
     console.log("NULL");
   }
 }
+
+// const node = new LinkedListNode(1);
+// const node2 = new LinkedListNode(2);
+// node.next = node2;
+
+// console.log(node)
 
 module.exports = LinkedList;
