5b417f8e2128534a85ff83985396249a497b1b88
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-09-01 11:18:57 -0500

    hash tables pt 1

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -12,14 +12,19 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
     // Your code here
+    return parseInt(sha256(key).slice(0, 8), 16);
   }
 
   hashMod(key) {
     // Your code here
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
