77549de475d1b8d380e77eedbc5e9d1a4c1dc7d9
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-09-22 11:58:19 -0500

    error handling lecture

--- week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
+++ week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
@@ -1,26 +1,49 @@
-// 1.
-function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
+// // 1.
+// function sum(array) {
+//   let sum = 0;
+//   try {
+//     for (let i = 0; i < array.length; i++) {
+//       sum += array[i];
+//     }
+//     return sum;
+//   } catch (error) {
+//     if (error instanceof TypeError) {
+//       console.error(error.message);
+//     }
+//   }
+// }
 
-let res = sum(null);
-console.log(res);
+// let res = sum(null);
+// console.log(res);
 
 // 2.
-// tests
-sayName("Alex");
-sayName(1);
-// Your code here
+// // Your code here
+// function sayName(name) {
+//   if (typeof name === 'string') {
+//     console.log(name);
+//   } else {
+//     throw TypeError("Invalid name! Must be a string!")
+//   }
+// }
+// // tests
+// try {
+//   sayName("Alex");
+//   sayName(1);
+// } catch (e) {
+//   console.error(e.message)
+// }
 
-// 3.
+// // 3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
 
   console.log(greeting);
+}
+
+try {
+  greet();
+} catch {
+  console.log('Hello World!');
 }
