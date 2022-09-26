42793b5775126606d6c5d405e6e461e970ee442d
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-09-26 18:19:44 -0500

    timing benchmarks phase 3

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
@@ -6,9 +6,14 @@ function addNums10Timing(increment) {
 
   const sums = [];
 
+  console.time('addNums');
   for (let i = increment; i <= increment * 100; i += increment) {
+    const start = Date.now();
     sums.push(addNums(i));
+    const end = Date.now();
+    console.log(end - start);
   }
+  console.timeEnd('addNums');
 
   return sums;
 
@@ -21,12 +26,26 @@ function addManyNums10Timing(increment) {
 
 const total = [];
 
+console.time('addManyNums');
 for (let i = increment; i <= increment * 100; i += increment) {
+  const start = Date.now();
   total.push(addManyNums(i));
+  const end = Date.now();
+  console.log(end - start);
 }
+console.timeEnd('addManyNums');
 
 return total;
 
 }
+
+let n = 100000;
+
+addNums10Timing(n);
+
+console.log('\n*****************\n');
+
+n = 1000
+addManyNums10Timing(n)
 
 
