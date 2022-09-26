cb6d495a7f51ff88e708f3fab2370c465ab32407
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-09-26 16:39:51 -0500

    timing benchmarks phase 1

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-1.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-1.js
@@ -1,15 +1,39 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
   // Fill this in
- 
-}
+  let sum = 0;  // c
+
+        // c              // c * n
+  for (let i = 0; i <= n; i++) {
+    // c * n
+    sum += i;
+  }
+  // c
+  return sum;
+} // O(n)
+
+// O(1 + 1 + (1 * n) +  (1 * n) + 1)
+// O(1 + 1 + n + n + 1)
+// O(n)
 
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
   // Fill this in
+  let sum = 0;  // c
 
-}
+        // c               c * n
+  for (let i = 1; i <= n; i++) {
+    // c * n   * n
+    sum += addNums(i);
+  }
+ // c
+  return sum;
+} // O(n^2)
+
+  // O(1 + 1 + (1 * n) + (1 * n * n) + 1);
+  // O(1 + 1 + n + n^2 + 1)
+  // O(n^2)
 
 
 
