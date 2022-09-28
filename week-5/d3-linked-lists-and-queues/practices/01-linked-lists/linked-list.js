188cba9546d22a9af9fb42e837abfb1f4989af75
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-09-28 11:43:33 -0500

    linked list lecture

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -8,14 +8,42 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);
+    
+    newNode.next = this.head;
+    this.head = newNode;
+
+    this.print();
+    this.length++;
   }
 
   addToTail(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);
+    this.length++;
+
+    // Case 1: There are not any nodes in the list
+    if (!this.head) {
+      this.head = newNode;
+      this.print()
+      return this;
+    }
+
+    // Case 2: If There are nodes in the list
+    let currentNode = this.head; // { val: 1, next: { val: 2, next: { val:3, next: null}}}
+
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = newNode;
+    this.print();
   }
 
   // You can use this function to help debug
