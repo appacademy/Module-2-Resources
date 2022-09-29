8dc03124258606922b1aeee307f4405644e55e58
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-09-29 11:07:20 -0500

    hash tables pt 1

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -1,5 +1,6 @@
 const sha256 = require('js-sha256');
 
+
 class KeyValuePair {
   constructor(key, value) {
     this.key = key;
@@ -12,14 +13,22 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
+    // console.log(this.data)
   }
 
   hash(key) {
     // Your code here
+    let sha = sha256(key);
+    // console.log(sha.slice(0,8))
+    return parseInt(sha.slice(0, 8), 16);
   }
 
   hashMod(key) {
     // Your code here
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
