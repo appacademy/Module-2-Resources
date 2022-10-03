2c2f632aa00bf23a2e4c427a41b10425c043203c
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-10-03 16:19:01 -0500

    Bubble sort lecture

--- week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
@@ -2,16 +2,36 @@
 function bubbleSort(arr) {
 
   // Iterate through the array
+  let hasSwapped = true;
 
-    // If the current value is greater than its neighbor to the right
+  while (hasSwapped) {
+    hasSwapped = false;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      // If the current value is greater than its neighbor to the right
+      if (arr[i] > arr[i+1]) {
+        // let temp = arr[i];
+        // arr[i] = arr[i+1];
+        // arr[i+1] = temp;
+
+        [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+        // Do not move this console.log
+        console.log(arr.join(","));
+  
+        hasSwapped = true;
+      }
       // Swap those values
 
-      // Do not move this console.log
-      console.log(arr.join(","));
+    }
+    if (!hasSwapped) return arr;
+    // If you get to the end of the array and no swaps have occurred, return
+  
+    // Otherwise, repeat from the beginning
 
-  // If you get to the end of the array and no swaps have occurred, return
 
-  // Otherwise, repeat from the beginning
+
+  }
+
 
 }
 
