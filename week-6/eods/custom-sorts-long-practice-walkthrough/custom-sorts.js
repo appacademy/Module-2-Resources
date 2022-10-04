b0be31c2eeef3f4d8cb1c2ceed6b829dcb8578cb
alex pezzati <aapezzati@gmail.com>
2022-10-04 17:52:55 -0400

    custom sorts long practice walkthrough setup

---
+++ week-6/eods/custom-sorts-long-practice-walkthrough/custom-sorts.js
@@ -0,0 +1,27 @@
+function ageSort(users) {
+  // Your code here
+}
+
+function oddEvenSort(arr) {
+  // Your code here
+}
+
+function validAnagrams(s, t) {
+  // Your code here
+}
+
+function reverseBaseSort(arr) {
+  // Your code here
+}
+
+function frequencySort(arr) {
+  // Your code here
+}
+
+module.exports = [
+  oddEvenSort,
+  validAnagrams,
+  reverseBaseSort,
+  frequencySort,
+  ageSort,
+];
