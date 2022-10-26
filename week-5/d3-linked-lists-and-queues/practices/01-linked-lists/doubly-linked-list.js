f58eb6df03bffe8f093e04ce4d4c82d51d3bb369
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-10-26 12:53:44 -0500

    doubly linked list lecture

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
@@ -9,14 +9,41 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+    this.length++;
+
+    // Case 1: There are NOT any nodes in the list
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
   }
 
   addToTail(val) {
     // Your code here
+    const newNode = new DoublyLinkedListNode(val);
+    this.length++;
+
+    // Case 1: There are NOT any nodes in the list
+    if (!this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   }
 
   // You can use this function to help debug
