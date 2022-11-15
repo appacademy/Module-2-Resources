3a1bd0ce852043ec1854de7e2ddab87b21aabd5c
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-11-15 11:56:21 -0600

    commonjs modules lecture

--- week-4/d2-es6-classes/practices/01-common-js-modules/send-messages/give-message-to-mrs-potato.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/send-messages/give-message-to-mrs-potato.js
@@ -1,8 +1,12 @@
-
+const sayHelloTo = require('./say-hello-to');
 
 function giveMessageToMrsPotato(message) {
   sayHelloTo("Mrs. Potato");
   console.log(`(Psst... ${message})`)
 }
 
+
 giveMessageToMrsPotato("Hi Buzz");
+
+
+module.exports = giveMessageToMrsPotato;
--- week-4/d2-es6-classes/practices/01-common-js-modules/send-messages/say-hello-to.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/send-messages/say-hello-to.js
@@ -1,7 +1,10 @@
-
+const sayMessage = require('../messages/say-message');
 
 function sayHelloTo(name) {
   sayMessage(`Hello ${name}!`)
 }
 
 sayHelloTo("Woody");
+
+
+module.exports = sayHelloTo ;
