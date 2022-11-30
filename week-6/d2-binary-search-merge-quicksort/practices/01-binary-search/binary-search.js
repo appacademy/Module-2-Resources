f292becf81ac2035fc8363af9673fbd4a71ed191
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-11-30 11:18:08 -0600

    binary search lecture

--- week-6/d2-binary-search-merge-quicksort/practices/01-binary-search/binary-search.js
+++ week-6/d2-binary-search-merge-quicksort/practices/01-binary-search/binary-search.js
@@ -1,29 +1,41 @@
 function linearSearch (arr, target) {
 
   // Can you solve this in one line?
-
+  return arr.indexOf(target);
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] === target) return i;
+  // }
+  // return -1;
 };
 
 function binarySearch(arr, target) {
 
   // Set integers pointing to the high and low range of possible indices
-
+  let hi = arr.length - 1;
+  let lo = 0;
+  let mid;
   // While high and low indices do not overlap...
-
+  while (lo <= hi) {
     // Find the midpoint between high and low indices
-
+    mid = Math.floor((hi + lo) / 2);
     // Compare the target value to the midpoint value
-
     // If the target equals the midpoint...
-      // Return the midpoint index
+    // Return the midpoint index
+    if (arr[mid] === target) return mid;
     // If the target is higher than the midpoint...
       // Move the low pointer to midpoint + 1
+    if (target > arr[mid]) lo = mid + 1 ;
     // If the target is less than the midpoint...
       // Move the high pointer to midpoint - 1
+    if (target < arr[mid]) hi = mid - 1;
+  }
+
 
   // Return -1 if the loop exits with overlapping pointers
-
+  return - 1;
 }
+//                 lm h    
+// [1, 2, 3, 4, 5, 6, 7, 8, 9] -> 6
 
 
 module.exports = [linearSearch, binarySearch]
