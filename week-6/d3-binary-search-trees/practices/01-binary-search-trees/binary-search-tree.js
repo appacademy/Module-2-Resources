ef68ffef8a9bc56e57f6928ac3eb4edf48f712f3
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-12-01 12:29:30 -0600

    bst recursive dfs

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -69,19 +69,70 @@ class BinarySearchTree {
     return false;
   }
 
+// The traversal strategy the programmer selects depends on the 
+// specific needs of the algorithm being designed. 
+// The goal is speed, so pick the strategy that brings you the nodes
+//  you require the fastest.
+
+// If you know you need to explore the roots before inspecting
+// any leaves, you pick pre-order because you will encounter 
+// all the roots before all of the leaves.
+
+// If you know you need to explore all the leaves before any nodes,
+//  you select post-order because you don't waste any time inspecting 
+// roots in search for leaves.
+
+// If you know that the tree has an inherent sequence in the nodes, 
+// and you want to flatten the tree back into its original sequence, 
+// than an in-order traversal should be used. 
+// The tree would be flattened in the same way it was created. 
+
+// A pre-order or post-order traversal might not unwind the tree back 
+// into the sequence which was used to create it.
+
+//      4
+//    /   \
+//   2     6
+//  / \   / \
+// 1   3 5   7
+
   // self, left, right
+  // 4, 2, 1, 3, 6, 5, 7
   preOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (!currentNode) return this;
+
+    console.log(currentNode.val);
+    this.preOrderTraversal(currentNode.left);
+    this.preOrderTraversal(currentNode.right);
   }
+
+//      4
+//    /   \
+//   2     6
+//  / \   / \
+// 1   3 5   7
+
+//1,2,3,4,5,6,7
 
   // left, self, right
   inOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (!currentNode) return this;
+
+    this.inOrderTraversal(currentNode.left);
+    console.log(currentNode.val);
+    this.inOrderTraversal(currentNode.right);
   }
 
   // left, right, self
   postOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (!currentNode) return this;
+
+    this.postOrderTraversal(currentNode.left);
+    this.postOrderTraversal(currentNode.right);
+    console.log(currentNode.val);
   }
 
     // Breadth First Traversal - Iterative
