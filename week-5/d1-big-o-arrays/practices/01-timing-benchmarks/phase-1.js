cd920a651a3504ff21ec210a87db942f14a2d65a
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-12-19 17:33:56 -0600

    timing benchmarks phases 1 and 2

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-1.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-1.js
@@ -1,13 +1,23 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
   // Fill this in
+  //    c
   let sum = 0;
 
+       //    c    (c * n) (c * n)
   for (let i = 1; i <= n; i++) {
+    // (c * n)
     sum += i;
   }
+  // c
   return sum;
 } // O(n)
+
+// T(c + c + (c * n) + (c * n) + (c * n) + c);
+// T(c + c + n + n + n + c);
+// T(n + n + n)
+// T(n * 3)
+// O(n)
 
 
 // Adds up values of addNums(1) through addNums(n)
--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
@@ -1,13 +1,26 @@
 const [addNums, addManyNums] = require("./phase-1");
 
+const arr = [];
+
+console.time('array')
+for (let i = 0; i < 10000; i++) {
+    const start = Date.now();
+    // console.log(start)
+    arr.push(addManyNums(i));
+    const end = Date.now();
+    console.log(end - start);
+}
+
+console.timeEnd('array');
 
 
-n = 1000
-console.log(`addNums(${n}): `);
-addNums10Timing(10000000);
 
-console.log("\n***********\n");
+// n = 1000
+// console.log(`addNums(${n}): `);
+// addNums10Timing(10000000);
 
-n = 1000
-console.log(`addManyNums(${n}): `);
-addManyNums10Timing(1000);
+// console.log("\n***********\n");
+
+// n = 1000
+// console.log(`addManyNums(${n}): `);
+// addManyNums10Timing(1000);
