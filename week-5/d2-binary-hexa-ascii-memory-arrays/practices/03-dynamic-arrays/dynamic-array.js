820d86406357d44ec903fae32b8b2e75c5f07e40
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-12-20 15:01:10 -0600

    dynamic array lecture

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
@@ -1,24 +1,32 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
-    // Your code here
-
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(this.capacity);
   }
 
   
 
   read(index) {
+    // if (index >= this.length) {
+    //   return undefined;
+    // }
 
-    // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
-
-    // Your code here
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i-1];
+    }
+    this.data[0] = val;
+    this.length++;
+    return this.length;
   }
 
 }
-
+//  i
+// [3,2,1,<>,<>,<>,<>,<>,]
 
 module.exports = DynamicArray;
