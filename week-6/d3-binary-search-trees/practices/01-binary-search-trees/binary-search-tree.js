0e9950fa2d752acb7bc3da43d0fa5b3cfd459f98
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-01-05 10:54:58 -0600

    bst insert

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -7,14 +7,48 @@ class TreeNode {
   }
 }
 
+//        4
+//       / \
+//      2   6
+//     / \ / \
+//    1  3 5  7
+
+
 class BinarySearchTree {
 
   constructor() {
     // Your code here
+    this.root = null;
   }
 
   insert(val, currentNode=this.root) {
     // Your code here
+    // create a new Node
+    const newNode = new TreeNode(val);
+    // if there is no currentNode place the newNode at the root
+    if (!currentNode) {
+      this.root = newNode;
+      return this;
+    }
+    // if the val is less than the currentNode's value
+    // look to the left
+    if (val < currentNode.val) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+      } else {
+        return this.insert(val, currentNode.left);
+      }
+    }
+
+    // if the val is greater than the currentNode's value
+    // look to the right
+    if (val > currentNode.val) {
+      if (!currentNode.right) {
+        currentNode.right = newNode;
+      } else {
+        return this.insert(val, currentNode.right);
+      }
+    }
   }
 
   search(val) {
