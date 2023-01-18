3dba370caa2cd3eb2f4618d0e21813d8bc948be7
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-01-17 19:08:23 -0600

    SRP DRY lecture

--- week-4/d1-basic-coding-principles/practices/03-dry/practice.js
+++ week-4/d1-basic-coding-principles/practices/03-dry/practice.js
@@ -24,6 +24,7 @@ function eatMostTacos(sum1, sum2) {
 }
 
 function adoptSmallerDog(weight1, weight2) {
+                  // weight1 === weight1
   let smallDog = findBiggerNum(weight1, weight2) === weight1 ? weight2 : weight1;
   
   return `I adopted a dog that weighs ${smallDog} pounds.`;
