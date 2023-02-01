722e435591fad44fedc53383725aff8b68ff2028
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-02-01 11:18:36 -0600

    bst insert

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -11,10 +11,43 @@ class BinarySearchTree {
 
   constructor() {
     // Your code here
+    this.root = null;
   }
+
+//        4
+//      /   \
+//     2     6
+//    / \   / \
+//   1   3 5   7
 
   insert(val, currentNode=this.root) {
     // Your code here
+    const newNode = new TreeNode(val);
+    // if there is no currentNode place the newNode at the root
+    if (!currentNode) {
+      this.root = newNode;
+      return this;
+    }
+
+    // if the passed val is less than the currentNode's value
+    // look to the left
+    if (val < currentNode.val) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+      } else {
+        return this.insert(val, currentNode.left);
+      }
+    }
+
+    // if the passed in val is greater than the currentNode's value
+    // look to the right
+    if (val > currentNode.val) {
+      if (!currentNode.right) {
+        currentNode.right = newNode;
+      } else {
+        return this.insert(val, currentNode.right);
+      }
+    }
   }
 
   search(val) {
@@ -44,7 +77,13 @@ class BinarySearchTree {
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
-}
+  }
 }
 
+
+//        4
+//      /   \
+//     2     6
+//    / \   / \
+//   1   3 5   7
 module.exports = { BinarySearchTree, TreeNode };
