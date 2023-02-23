353ff304b462238e941367159bd82f190469301b
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-02-23 11:41:58 -0600

    hashing functions

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -12,14 +12,26 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
     // Your code here
+    // const hash = sha256(key);
+    // const first8 = hash.slice(0,8);
+    // const decimal = parseInt(first8, 16);
+    // return decimal;
+    return parseInt(sha256(key).slice(0,8), 16);
   }
 
   hashMod(key) {
     // Your code here
+    // const hash = this.hash(key);
+    // const index = hash % this.capacity;
+    // return index;
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
@@ -35,6 +47,11 @@ class HashTable {
   }
 
 }
-
+// console.log(parseInt(sha256('abnskduealnvas;dfhdkk').slice(0,8),16))
+// console.log(sha256('A'))
+// console.log(sha256('b'))
+// console.log(sha256('B'))
+// console.log(sha256('ab'))
+// console.log(sha256('AB'))
 
 module.exports = HashTable;
