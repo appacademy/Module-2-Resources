24b8d13c5c5bb32a4c012159d3108aa8abc55945
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-03-02 12:41:09 -0600

    a shortest path

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
@@ -9,12 +9,40 @@ const adjList = {
 
 function aShortestPath(start, end) {
   // Your code here
-  
+  const queue = [[start]];
+  const visited = new Set([start]);
+  console.log({queue})
+  console.log({visited})
+
+  while (queue.length) {
+    const currentPath = queue.shift();
+    const currentNode = currentPath[currentPath.length - 1];
+
+    if (currentNode === end) return currentPath;
+    // queue = [[[1,5,4]]
+    // visited = {1,2,5,3,4}
+    // currentPath =  [1,2,3]
+    // currentNode = 3
+    // target = 3
+    //                    n
+    // neighbors = [1, 2, 4]
+
+    adjList[currentNode].forEach(neighbor => {
+      if (!visited.has(neighbor)) {
+        queue.push([...currentPath, neighbor]);
+        visited.add(neighbor);
+        console.log({queue})
+        console.log({visited})
+      }
+    });
+  }
+  return false;
 }
+
 
 console.log("First Test:");
 console.log(aShortestPath(1, 3)); // -> [ 1, 2, 3 ] (One possible solution)
-console.log("Second Test:");
-console.log(aShortestPath(4, 1)); // -> [ 4, 5, 1 ] (One possible solution)
-console.log("Third Test:");
-console.log(aShortestPath(6, 1)); // -> false
+// console.log("Second Test:");
+// console.log(aShortestPath(4, 1)); // -> [ 4, 5, 1 ] (One possible solution)
+// console.log("Third Test:");
+// console.log(aShortestPath(6, 1)); // -> false
