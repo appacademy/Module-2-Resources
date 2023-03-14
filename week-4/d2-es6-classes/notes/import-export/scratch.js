16c21f25a2badf90386172ef6de3e7dcb30bed0f
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-03-14 10:46:15 -0500

    morning modules lecture

---
+++ week-4/d2-es6-classes/notes/import-export/scratch.js
@@ -0,0 +1,8 @@
+console.log(module.exports);
+const hello = 'hello';
+const world = 'world';
+
+module.exports = { hello, world };
+// module.exports.hello = hello;
+// module.exports.world = world;
+console.log(module);
