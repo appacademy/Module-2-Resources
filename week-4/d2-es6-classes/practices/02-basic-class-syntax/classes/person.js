7d499a483c5037f81589ce583d769afcb41f6880
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-03-14 13:16:02 -0500

    person class syntax, working on introducePeople

--- week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
+++ week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
@@ -1,4 +1,25 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople(people) {
+    people.forEach(person => person.introduce());
+  }
+}
+
+// const kristen = new Person("kristen", "chauncey", 38);
+// const shane = new Person('shane', 'wilkey', 41);
+// kristen.introduce();
+// shane.introduce();
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
