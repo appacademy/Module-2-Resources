a394ac4f5c070bfd04cbef2c4e0d57769351b085
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-03-22 12:05:03 -0500

    singly linked lists

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -8,14 +8,41 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
+    // console.log(this)
+    const newNode = new LinkedListNode(val);
+
+    newNode.next = this.head; // => this may be null
+    this.head = newNode;
+    this.length++;
+    // console.log(this)
+    // this.print()
+    return this;
     // Your code here
   }
 
   addToTail(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);
+    this.length++;
+
+    // if there is no head
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      
+      currentNode.next = newNode;
+    }
+    return this
   }
 
   // You can use this function to help debug
