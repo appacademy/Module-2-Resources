6359ed274ac17cdbdf7aae9706c45c8421a354cb
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-03-29 15:04:12 -0500

    bst breadth and depth first traversals

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -171,15 +171,53 @@ class BinarySearchTree {
         /   \   /   \
        1     3 5     7
 */
+  // queue = []
+  // curr = 7
+  // cl => 4,2,6,1,3,5,7
     // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
     // your code here
     const queue = [];
+    queue.push(this.root);
+
+    while (queue.length) {
+      const curr = queue.shift();
+
+      // DO THE THING!!!
+      console.log(curr.val);
+
+      if (curr.left) queue.push(curr.left);
+      if (curr.right) queue.push(curr.right);
+    }
+    return this;
   }
 
+/*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+*/
+  // stack = []
+  // curr = 1
+  // cl => 4,6,7,5,2,3,1
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
+    const stack = [];
+    stack.push(this.root);
+
+    while (stack.length) {
+      const curr = stack.pop();
+
+      // DO THE THING!!!
+      console.log(curr.val);
+
+      if (curr.left) stack.push(curr.left);
+      if (curr.right) stack.push(curr.right);
+    }
+    return this;
   }
 }
 
