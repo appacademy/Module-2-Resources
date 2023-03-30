42ad86d00ea753a57e6717f6e88497155b7867e9
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-03-30 10:40:36 -0500

    morning graphs lecture

--- week-6/d4-graphs/practices/01-graph-repl-problems/homework/01-simple-breadth-first-traversal.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/homework/01-simple-breadth-first-traversal.js
@@ -9,14 +9,38 @@ const adjList = {
 
 function printBreadthFirst(start) {
     // your code here
+    const queue = []
+    queue.push(start);
+    console.log({ queue })
+    const visited = new Set();
+    visited.add(start);
+    console.log({ visited })
+
+    while (queue.length) {
+        const curr = queue.shift();
+
+        // DO THE THING
+        console.log(curr);
+
+        const neighbors = adjList[curr];
+        neighbors.forEach(neighbor => {
+            if (!visited.has(neighbor)) {
+                queue.push(neighbor);
+                console.log({ queue })
+                visited.add(neighbor);
+                console.log({ visited })
+            }
+        });
+    }
+    return;
 }
 
 console.log("First Test:")
 printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
                     // One possible solution:  3, 2, 4, 1, 5, 6
-console.log("Second Test:")
-printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-                    // One possible solution:  6, 4, 3, 5, 2, 1
-console.log("Third Test:")
-printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
-                    // One possible solution:  4, 3, 5, 6, 2, 1
+// console.log("Second Test:")
+// printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
+//                     // One possible solution:  6, 4, 3, 5, 2, 1
+// console.log("Third Test:")
+// printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
+//                     // One possible solution:  4, 3, 5, 6, 2, 1
