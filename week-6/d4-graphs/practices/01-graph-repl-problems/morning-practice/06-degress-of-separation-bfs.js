b899af9cb003c5739d495bf1c2e8e91270ba2f98
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-03-30 15:26:07 -0500

    matrix lecture

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/06-degress-of-separation-bfs.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/06-degress-of-separation-bfs.js
@@ -9,6 +9,24 @@ const adjList = {
 
 function degreesOfSeparation(start, end) {
   // Your code here
+  const queue = [[start]];
+  const visited = new Set([start]);
+  console.log(visited)
+
+  while (queue.length) {
+    const currentPath = queue.shift();
+    const currentNode = currentPath[currentPath.length - 1];
+
+    if (currentNode === end) return currentPath.length - 1;
+
+    adjList[currentNode].forEach(neighbor => {
+      if (!visited.has(neighbor)) {
+        queue.push([...currentPath, neighbor]);
+        visited.add(neighbor);
+      }
+    });
+  }
+  return false;
 }
 
 console.log("First Test:");
