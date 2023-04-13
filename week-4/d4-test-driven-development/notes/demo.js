8535fa9fe87c34031717455bf3880828ab25b881
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-04-13 10:50:00 -0500

    morning lecture

--- week-4/d4-test-driven-development/notes/demo.js
+++ week-4/d4-test-driven-development/notes/demo.js
@@ -1,30 +1,71 @@
-// funtion add(num1, num2){
-//     console.log(num1 + num2)
-// }
+class NotNumberError extends Error {
+    constructor(...params) {
+        super(params )
+    }
+}
 
-// add(1, 2)
+
+function add(num1, num2){
+    try {
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            throw new NotNumberError('Input must be a number');
+        }
+    } catch (e) {
+        console.error(e.message)
+    }
+    console.log(num1 + num2)
+}
+
+add('1', 2)
+// try {
+//     add(1, 2);
+//     // add(1, '2')
+
+// } catch (error) {
+//     console.error(error)
+//     // console.log('hello world')
+// }
 
 // const puppy = "puppy";
 // console.log(pupppy); 
 
-// function sumDiv(n1, n2) {
-    
-    
+// try {
+//     function callPuppy() {
+//         const puppy = "puppy";
+//     }
+//     console.log(puppy); 
+// } catch(error) {
+//     console.error(error);
 // }
 
-// console.log(sumDiv())
 
-function funStuff(str) {
-    const arr = str.split('')
-    return arr;
-}
+// let dog; // Remember unassigned variables are undefined!
+// dog(); // TypeError: dog is not a function
 
-try {
-    funStuff(4)
-} catch (err) {
-    console.log(err.message)
-    // throw new Error('what are you doing?')
-}
+// const puppy = "puppy";
+// puppy = "apple"; // TypeError: Assignment to constant variable.
 
-const taco = new Array(4).fill(null)
-console.log(taco)
+// function funStuff(str) {
+//     const arr = str.split('')
+//     return arr;
+// }
+
+// try {
+//     funStuff(4)
+// } catch (err) {
+//     console.log(err.message)
+//     // throw new Error('what are you doing?')
+// }
+
+
+
+// function login(username, password) {
+//     try {
+//         // send this info to the server
+//         // log the user in
+//     } catch(e) {
+//         // send error message back to the user
+//     }
+// }
+
+// "Incorrect username or password"
