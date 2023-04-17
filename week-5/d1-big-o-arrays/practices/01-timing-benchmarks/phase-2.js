fd91d01757db897551bed3be67ea534f86739fc4
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-04-17 18:05:14 -0500

    timing benchmarks

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
@@ -3,15 +3,27 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
+  const res = [];
 
+  for (let i = 1; i <= 10; i++) {
+    res.push(addNums(i * increment))
+  }
 
-}
+  return res;
+
+} // O(n)
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
+  const res = [];
 
+  for (let i = 1; i <= 10; i++) {
+    res.push(addManyNums(i * increment));
+  }
 
-}
+  return res;
+
+} // O(n^2)
 
 module.exports = [addNums10, addManyNums10];
