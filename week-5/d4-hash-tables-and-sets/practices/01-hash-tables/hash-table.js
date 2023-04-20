89a1201ffc03c32914800243551d368d20943dc0
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-04-20 11:23:47 -0500

    hash and hash mod methods

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -12,18 +12,36 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
     // Your code here
+    // const hash = sha256(key);
+    // const first8 = hash.slice(0,8);
+    // const integer = parseInt(first8, 16)
+    // return integer;
+    return parseInt(sha256(key).slice(0,8), 16)
   }
 
   hashMod(key) {
     // Your code here
+    // const hashInteger = this.hash(key);
+    // const index = hashInteger % this.capacity;
+    // return index;
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
     // Your code here
+    const newNode = new KeyValuePair(key, value);
+
+    // if there is a node at this index throw an error
+    // this.data[index] == this.head
+
+    this.count++
   }
 
   insertWithHashCollisions(key, value) {
@@ -35,6 +53,10 @@ class HashTable {
   }
 
 }
+
+// console.log(sha256('Shane'));
+// console.log(sha256('shane'));
+
 
 
 module.exports = HashTable;
