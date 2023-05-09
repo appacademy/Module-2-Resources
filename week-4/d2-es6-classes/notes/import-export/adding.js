82943a61ddc512bf118aa9d4abffaf14fb72aa62
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-05-09 10:41:25 -0500

    morning lecture

--- week-4/d2-es6-classes/notes/import-export/adding.js
+++ week-4/d2-es6-classes/notes/import-export/adding.js
@@ -1,17 +1,17 @@
-exports.adding = function adding(a, b) {
+function adding(a, b) {
     return a + b;
 }
-exports.subtraction = function subtraction(a, b) {
+function subtraction(a, b) {
   if (a > b) return a - b;
   return b - a;
 }
-exports.division = (num1, num2) => {
+const division = (num1, num2) => {
   return num1 / num2;
 }
 //this function returns the sum of two numbers
-// module.exports = {
-//     adding, subtraction, division
-// }
+module.exports = {
+    adding, subtraction, division
+}
 // module.exports = {
 //     adding: adding
 // }
