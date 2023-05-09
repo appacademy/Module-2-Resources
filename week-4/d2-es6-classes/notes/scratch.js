b9aa00647b26e1f4363f11c2022de2802ab18f27
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-05-09 11:55:16 -0500

    commonJS modules

---
+++ week-4/d2-es6-classes/notes/scratch.js
@@ -0,0 +1,52 @@
+// const user = {
+//     name: 'shane',
+//     occupation: 'instructor',
+//     age: 41,
+//     introduce:  () => console.log(`Hi, my name is ${this.name}.`)
+// }
+
+// console.log(user)
+// const user2 = {
+//     name: 'Gary', 
+//     occupation: 'instructor',
+//     age: 21,
+//     introduce:  () => console.log(`Hi, my name is ${this.name}.`)
+// }
+
+
+class User {
+    constructor(name, occupation, age) {
+        this.name = name;
+        this.occupation = occupation;
+        this.age = age;
+    }
+
+    introduce() {
+        console.log(`Hi, my name is ${this.name}.`)
+    }
+
+    static introduceUsers(bananas) {
+        bananas.forEach(banana => banana.introduce());
+    }
+}
+
+const shane = new User('shane', 'instructor', 41);
+// console.log(shane)
+shane.introduce()
+
+const gary = new User('gary', 'instructor', 21);
+// console.log(gary)
+
+
+gary.introduce()
+User.introduceUsers([shane, gary]);
+
+const arr = [1,2,3];
+
+arr.push(4);
+
+const arr2 = new Array()
+
+console.log(Array.isArray(arr2))
+
+
--- week-4/d2-es6-classes/practices/01-common-js-modules/index.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/index.js
@@ -1,6 +1,7 @@
-
-// Your code here
-
+const sayHelloTo = require('./send-messages/say-hello-to');
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
+const { message1: msg1, message2: msg2, message3: msg3 } = require('./messages');
+// const { msg1, msg2, msg3 } = require('./messages');
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
