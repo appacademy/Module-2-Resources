504afdbb31a511218a5a353da4b04e023ffb7dc1
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-05-10 15:50:32 -0500

    context with arrow functions

--- week-4/d3-context-bind-call-apply/practices/04-context-arrow-functions/classes/user.js
+++ week-4/d3-context-bind-call-apply/practices/04-context-arrow-functions/classes/user.js
@@ -3,10 +3,47 @@ class User {
     this.name = name;
   }
 
-  changeName(newName) {
+  changeName = (newName) => {
     this.name = newName;
     return this.name;
-  }
+  };
 }
+
+// const user1 = new User("shane");
+// const user2 = new User("briana");
+// const user3 = new User("gary");
+// const user4 = new User("charlie");
+// const user5 = new User("roger");
+
+// console.log({ user1 });
+// console.log({ user2 });
+// console.log({ user3 });
+// console.log({ user4 });
+// console.log({ user5 });
+
+// user1.changeName("kristen");
+// console.log({ user1 });
+
+// const obj = {
+//   name: "shane",
+//   sayName: function () {
+//     console.log(this);
+//     console.log(this.name);
+//   },
+// };
+
+// obj.sayName();
+
+// const obj2 = {
+//   name: "briana",
+// };
+
+// const sN = obj.sayName.bind(obj2);
+
+// sN();
+
+// obj2.sayName();
+
+// obj.sayName();
 
 module.exports = User;
--- week-4/d3-context-bind-call-apply/practices/04-context-arrow-functions/review.js
+++ week-4/d3-context-bind-call-apply/practices/04-context-arrow-functions/review.js
@@ -1,10 +1,10 @@
 // Convert restSum into a multi-line fat-arrow function
-const restSum = function(...otherNums) {
+const restSum = (...otherNums) => {
   let sum = otherNums.reduce((acc, el) => (acc += el));
   return sum;
 };
 
-console.log('restSum output:');
+console.log("restSum output:");
 console.log(restSum(1, 2, 3)); // 6
 
 // Convert getFullName into a one-liner fat-arrow function with implicit return
@@ -12,9 +12,7 @@ console.log(restSum(1, 2, 3)); // 6
 //   person.firstName + " " + person.lastName;
 // };
 
-const getFullName = function(person) {
-  return person.firstName + " " + person.lastName
-};
+const getFullName = (person) => person.firstName + " " + person.lastName;
 
 class Person {
   constructor(firstName, lastName) {
@@ -23,25 +21,20 @@ class Person {
   }
 }
 
-console.log('getFullName output:');
+console.log("getFullName output:");
 console.log(getFullName(new Person("Elle", "Woods"))); // Elle Woods
 
 // Convert addFive into a one-liner fat-arrow function with implicit return
-const addFive = function(num){
-  num + 5
-};
+const addFive = (num) => num + 5;
 
-console.log('addFive output:');
+console.log("addFive output:");
 console.log(addFive(3)); // 8
 
 // Convert coupon and the function it returns into fat-arrow functions
-function coupon(discount) {
-  return function(prices) {
-    return prices.map((price) => (price -= price * discount));
-  };
-}
+const coupon = (discount) => (prices) =>
+  prices.map((price) => (price -= price * discount));
 
-console.log('coupon output:');
+console.log("coupon output:");
 console.log(coupon(0.5)([10, 6, 3, 9])); // [ 5, 3, 1.5, 4.5 ]
 
 // Challenge: Can you define the coupon function and its returned function in
