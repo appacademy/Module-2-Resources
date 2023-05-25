ffc469b4bd2f51cea709cea530e653caa8765de9
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-05-25 14:40:03 -0500

    shortest path, degrees, matrix lecture

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
@@ -9,11 +9,32 @@ const adjList = {
 
 function aShortestPath(start, end) {
   // Your code here
+  const queue = [[start]];
+  const visited = new Set([start]);
+
+  while (queue.length) {
+    //  [[1,5,4]]
+    const currPath = queue.shift(); // [1, 2, 3]
+    const currNode = currPath[currPath.length - 1]; // 3
+
+    // DO THE THING
+    if (currNode === end) return currPath; // [1,2,3]
+
+    adjList[currNode].forEach((neighbor) => {
+      // [1, 2, 4]
+      if (!visited.has(neighbor)) {
+        // {1,2,5,3,4}
+        queue.push([...currPath, neighbor]);
+        visited.add(neighbor);
+      }
+    });
+  }
+  return false;
 }
 
 console.log('First Test:');
 console.log(aShortestPath(1, 3)); // -> [ 1, 2, 3 ] (One possible solution)
-console.log('Second Test:');
-console.log(aShortestPath(4, 1)); // -> [ 4, 5, 1 ] (One possible solution)
-console.log('Third Test:');
-console.log(aShortestPath(6, 1)); // -> false
+// console.log('Second Test:');
+// console.log(aShortestPath(4, 1)); // -> [ 4, 5, 1 ] (One possible solution)
+// console.log('Third Test:');
+// console.log(aShortestPath(6, 1)); // -> false
