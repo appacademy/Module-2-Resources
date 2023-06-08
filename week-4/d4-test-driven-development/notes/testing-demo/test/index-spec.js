f30f8caa5398851b78abb9beae330438bb7e1c8d
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-06-08 12:22:26 -0500

    error handling specs lecture

---
+++ week-4/d4-test-driven-development/notes/testing-demo/test/index-spec.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const message = require('../index');
+
+describe('the index file', () => {
+  it('should have a message', () => {
+    expect(message).to.equal('Hello World From Test Directory');
+  });
+});
