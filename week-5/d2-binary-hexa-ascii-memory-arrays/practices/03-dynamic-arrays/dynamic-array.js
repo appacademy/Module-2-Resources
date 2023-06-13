a70c38e7d4f3b9287c59b529f2fcccf5fb5506ce
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-06-13 12:58:05 -0500

    advanced conversions

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
@@ -1,22 +1,15 @@
 class DynamicArray {
-
-  constructor(defaultSize=4) {
-
+  constructor(defaultSize = 4) {
     // Your code here
-
   }
 
   read(index) {
-
     // Your code here
   }
 
   unshift(val) {
-
     // Your code here
   }
-
 }
-
 
 module.exports = DynamicArray;
