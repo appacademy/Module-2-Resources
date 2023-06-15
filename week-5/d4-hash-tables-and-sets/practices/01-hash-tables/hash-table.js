9f6a196e7d84c39d6e50fd368086f5a746700dd0
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-06-15 11:26:47 -0500

    hash and hash mod methods

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -9,21 +9,35 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
+    // console.log(this.data);
   }
 
   hash(key) {
     // Your code here
+    // let hash = sha256(key);
+    // hash = hash.slice(0, 8);
+    // const decimal = parseInt(hash, 16);
+    // return decimal;
+    return parseInt(sha256(key).slice(0, 8), 16);
   }
 
   hashMod(key) {
     // Your code here
+    // const hashedInteger = this.hash(key);
+    // const index = hashedInteger % this.capacity;
+    // return index;
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
     // Your code here
+    const newNode = new KeyValuePair(key, value);
+    console.log(newNode);
   }
 
   insertWithHashCollisions(key, value) {
@@ -33,8 +47,11 @@ class HashTable {
   insert(key, value) {
     // Your code here
   }
-
 }
 
+// const hash = sha256('shane');
+// const hash2 = sha256('Shane');
+// console.log(parseInt(hash.slice(0, 8), 16) % 4);
+// console.log(hash2);
 
 module.exports = HashTable;
