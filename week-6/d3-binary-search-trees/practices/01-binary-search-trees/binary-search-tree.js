5984d9705a08337ec01df4d9dab0cbaed03858e5
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-06-21 12:49:00 -0500

    pre in post order traversal bst

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -76,52 +76,118 @@ class BinarySearchTree {
   }
 
   search2(val, curr = this.root) {
-    if (!curr) return false;
-    if (curr.val === val) return true;
-    return val < curr.val
+    // if (!curr) return false;
+    // if (curr.val === val) return true;
+    // return val < curr.val
+    //   ? this.search2(val, curr.left)
+    //   : this.search2(val, curr.right);
+    return !curr
+      ? false
+      : curr.val === val
+      ? true
+      : val < curr.val
       ? this.search2(val, curr.left)
       : this.search2(val, curr.right);
-    // return !curr
-    //   ? false
-    //   : curr.val === val
-    //   ? true
-    //   : val < curr.val
-    //   ? this.search2(val, curr.right)
-    //   : this.search2(val, curr.left);
   }
 
+  // The traversal strategy the programmer selects depends on the
+  // specific needs of the algorithm being designed.
+  // The goal is speed, so pick the strategy that brings you the nodes
+  //  you require the fastest.
+
+  // If you know you need to explore the roots before inspecting
+  // any leaves, you pick pre-order because you will encounter
+  // all the roots before all of the leaves.
+
+  // If you know you need to explore all the leaves before any nodes,
+  //  you select post-order because you don't waste any time inspecting
+  // roots in search for leaves.
+
+  // If you know that the tree has an inherent sequence in the nodes,
+  // and you want to flatten the tree back into its original sequence,
+  // than an in-order traversal should be used.
+  // The tree would be flattened in the same way it was created.
+
+  // A pre-order or post-order traversal might not unwind the tree back
+  // into the sequence which was used to create it.
+
+  /*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+*/
+  // cl => 4,2,1,3,6,5,7
   // self, left, right
   preOrderTraversal(currentNode = this.root) {
+    if (!currentNode) return;
     // Your code here
     // DO THE THING
+    console.log(currentNode.val);
     // go left
+    this.preOrderTraversal(currentNode.left);
     // go right
+    this.preOrderTraversal(currentNode.right);
   }
-
+  /*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+*/
+  // cl => 1,2,3,4,5,6,7
   // left, self, right
   inOrderTraversal(currentNode = this.root) {
     // Your code here
+    if (!currentNode) return;
     // go left
+    this.inOrderTraversal(currentNode.left);
     // DO THE THING
+    console.log(currentNode.val);
     // go right
+    this.inOrderTraversal(currentNode.right);
   }
 
+  /*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+*/
+  // cl => 1,3,2,5,7,6,4
   // left, right, self
   postOrderTraversal(currentNode = this.root) {
+    if (!currentNode) return;
     // Your code here
     // go left
+    this.postOrderTraversal(currentNode.left);
     // go right
+    this.postOrderTraversal(currentNode.right);
     // DO THE THING
+    console.log(currentNode.val);
   }
+
+  /*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+*/
 
   // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
     // your code here
+    const queue = [this.root];
   }
 
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
+    const stack = [this.root];
   }
 }
 
@@ -135,7 +201,10 @@ bst.insert(3);
 bst.insert(5);
 bst.insert(7);
 
+// console.log(bst.search2(8));
+console.log(bst.search2(4));
+console.log(bst.search2(5));
+console.log(bst.search2(1));
 console.log(bst.search2(8));
-console.log(bst.search2(2));
 
 module.exports = { BinarySearchTree, TreeNode };
