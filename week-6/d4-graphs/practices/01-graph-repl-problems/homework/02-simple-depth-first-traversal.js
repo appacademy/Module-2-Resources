721f2b10b530876d94c59ce5771e4e5343442095
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-06-22 11:07:34 -0500

    refactor BFT

--- week-6/d4-graphs/practices/01-graph-repl-problems/homework/02-simple-depth-first-traversal.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/homework/02-simple-depth-first-traversal.js
@@ -1,22 +1,46 @@
 const adjList = {
-    1: [2, 5],
-    2: [1, 3, 5],
-    3: [2, 4],
-    4: [3, 5, 6],
-    5: [1, 2, 4],
-    6: [4]
-}
+  1: [2, 5],
+  2: [1, 3, 5],
+  3: [2, 4],
+  4: [3, 5, 6],
+  5: [1, 2, 4],
+  6: [4],
+};
 
 function printDepthFirst(start) {
-    // your code here
+  const stack = [start];
+  const visited = new Set([start]);
+  //   console.log({ stack });
+  //   console.log({ visited });
+  // visited.add(start)
+  while (stack.length) {
+    // []
+    const curr = stack.pop(); // 6
+
+    // DO THE THING
+    console.log(curr); // cl => 3,2,4,1,5,6
+
+    const neighbors = adjList[curr];
+    // console.log(neighbors);             i
+    neighbors.forEach((neighbor) => {
+      // [4]
+      if (!visited.has(neighbor)) {
+        stack.push(neighbor);
+        visited.add(neighbor); // {3,2,4,1,5,6}
+        // console.log({ stack });
+        // console.log({ visited });
+      }
+    });
+  }
+  return;
 }
 
-console.log("First Test:")
+console.log('First Test:');
 printDepthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
-                    // One possible solution:  3, 4, 6, 5, 1, 2
-console.log("Second Test:")
+// One possible solution:  3, 4, 6, 5, 1, 2
+console.log('Second Test:');
 printDepthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-                    // One possible solution:  6, 4, 5, 2, 1, 3
-console.log("Third Test:")
+// One possible solution:  6, 4, 5, 2, 1, 3
+console.log('Third Test:');
 printDepthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
-                    // One possible solution:  4, 6, 5, 2, 1, 3
+// One possible solution:  4, 6, 5, 2, 1, 3
