7cb4ac0d824db60aa472139ba196dd936c8439f1
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-06-22 11:41:04 -0500

    simple bfs

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
@@ -4,16 +4,16 @@ const adjList = {
   3: [2, 4],
   4: [3, 5],
   5: [1, 2, 4],
-  6: []
-}
+  6: [],
+};
 
 function aShortestPath(start, end) {
   // Your code here
 }
 
-console.log("First Test:");
+console.log('First Test:');
 console.log(aShortestPath(1, 3)); // -> [ 1, 2, 3 ] (One possible solution)
-console.log("Second Test:");
+console.log('Second Test:');
 console.log(aShortestPath(4, 1)); // -> [ 4, 5, 1 ] (One possible solution)
-console.log("Third Test:");
+console.log('Third Test:');
 console.log(aShortestPath(6, 1)); // -> false
