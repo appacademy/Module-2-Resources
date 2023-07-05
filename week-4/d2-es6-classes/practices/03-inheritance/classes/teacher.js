75a8a82d51a4330845076b2994ad9c75632fb3f4
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-07-05 15:50:47 -0500

    inheritance/polymorphism

--- week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
@@ -1,6 +1,22 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    // let sum = 0;
+    // for (let teacher of teachers) {
+    //   sum += teacher.yearsOfExperience;
+    // }
+    // return sum;
+    return teachers.reduce((sum, teacher) => sum + teacher.yearsOfExperience, 0)
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
