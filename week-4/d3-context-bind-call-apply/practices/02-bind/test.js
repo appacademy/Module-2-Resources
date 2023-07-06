e848dc1dc1823e9cb3b87999419591a810b5898c
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-07-06 12:47:22 -0500

    bind lecture

--- week-4/d3-context-bind-call-apply/practices/02-bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/test.js
@@ -1,1 +1,19 @@
 // Your code here
+const Employee = require('./employee');
+
+const john = new Employee('John Wick', 'Dog Lover');
+// sayNameFunc
+setTimeout(john.sayName.bind(john), 2000);
+setTimeout(john.sayOccupation.bind(john), 3000);
+
+// let a = setTimeout(john.sayName, 2000);
+// // console.log(a)
+// a.name = john.name
+// console.log(a)
+
+// setTimeout(function () {
+//   john.sayName()
+// }, 2000)
+
+// console.log(john.sayName())
+// console.log(john.sayOccupation())
