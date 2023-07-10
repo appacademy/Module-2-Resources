fcd6ccf759a9c77e97311f85411cbe648c4ca628
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-07-10 17:50:47 -0500

    timing benchmarks phase 3

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
@@ -1,13 +1,42 @@
 const [addNums, addManyNums] = require("./phase-1");
 
+function addNums10Timing(increment) {
+  // Fill this in
+  const arr = [];
+  for (let i = 1; i <= 100; i++) {
+    const start = Date.now()
+    arr.push(addNums(i * increment));
+    const end = Date.now()
+    console.log(end - start)
+  }
+  return arr;
+}
+
+// Runs `addManyNums` in 10 increasing increments
+function addManyNums10Timing(increment) {
+  // Fill this in
+  const arr = [];
+  for (let i = 1; i <= 100; i++) {
+    const start = Date.now()
+    arr.push(addManyNums(i * increment));
+    const end = Date.now()
+    console.log(end - start)
+  }
+  return arr;
+}
 
 
 n = 1000
 console.log(`addNums(${n}): `);
+console.time('addNums100')
 addNums10Timing(10000000);
+console.timeEnd('addNums100')
 
 console.log("\n***********\n");
 
 n = 1000
+
 console.log(`addManyNums(${n}): `);
-addManyNums10Timing(1000);
+console.time('addManyNums100')
+addManyNums10Timing(5000);
+console.timeEnd('addManyNums100')
