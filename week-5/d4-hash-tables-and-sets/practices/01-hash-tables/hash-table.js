a5de618ade22507120fcdee94dbc1a14257e0eb0
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-07-13 11:35:48 -0500

    hash tables hashing functions

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -12,14 +12,27 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
     // Your code here
+    // let hash = sha256(key)
+    // hash = hash.substring(0,8);
+    // hash = parseInt(hash, 16);
+    // // console.log({hash})
+    // return hash;
+    return parseInt(sha256(key).substring(0, 8), 16)
   }
 
   hashMod(key) {
     // Your code here
+    let hash = this.hash(key);
+    let index = hash % this.capacity;
+    console.log({index})
+    return index
   }
 
   insertNoCollisions(key, value) {
@@ -36,5 +49,10 @@ class HashTable {
 
 }
 
+// const hash = sha256('a');
+// const hash2 = sha256('A42354');
 
+
+// console.log(parseInt(hash.substring(0, 8), 16) % 2)
+// console.log(parseInt(hash2.substring(0, 8), 16) % 2)
 module.exports = HashTable;
