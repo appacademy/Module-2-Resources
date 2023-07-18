d640faa21f62c0da51b9e97272de6e2c43e6fe48
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-07-18 09:51:24 -0500

    selection sort

--- week-6/d1-sorting-bubble-insertion-selection/practices/03-selection-sort/selection-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/03-selection-sort/selection-sort.js
@@ -34,16 +34,16 @@ function selectionSortInPlace(arr) { // [2,4,6,8,1,3,5,7,9]
   // Repeat while the unsorted half is not empty:
   while (divider < arr.length) {
     // Do not move this console.log
-    //                               d               
-    console.log(arr.join(","));  // [2,2,4,6,8,3,5,7,9]
-    //                                       m
+    //                                     d i               
+    console.log(arr.join(","));  // [1,2,3,4,6,8,5,7,9]
+    //                                     m
     // Find the index of the minimum value in the unsorted half
     let minIndex = divider;
     for (let i = minIndex + 1; i < arr.length; i++) {
       if (arr[i] < arr[minIndex]) minIndex = i;
     }
     // Save the min value
-    const val = arr[minIndex]; // 1
+    const val = arr[minIndex]; // 3
     // Shift every unsorted value to the left of the min value to the right by 1
     for (let i = minIndex; i > divider; i--) {
       arr[i] = arr[i-1];
