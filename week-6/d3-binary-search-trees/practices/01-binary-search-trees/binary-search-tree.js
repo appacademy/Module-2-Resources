d4a120d80bc0b7a342cffb867c3c38cefec7d99c
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-07-19 12:03:26 -0500

    bst search

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -47,25 +47,65 @@ class BinarySearchTree {
         return this.insert(val, currentNode.right);
       }
     }
+
   }
 
   search(val) {
     // Your code here
+    let curr = this.root;
+
+    while (curr) {
+      if (curr.val === val) return true;
+      curr = val < curr.val ? curr.left : curr.right
+
+      // if (curr.val < val) {
+      //   curr = curr.right;
+      // } else {
+      //   curr = curr.left;
+      // }
+    }
+    return false;
   }
 
+  search2(val, curr=this.root) {
+    // if (!curr) return false;
+    // if (curr.val === val) return true;
 
+    // return val < curr.val 
+    //   ? this.search2(val, curr.left) 
+    //   : this.search2(val, curr.right);
+
+    return !curr
+      ? false
+      : curr.val === val
+      ? true
+      : val < curr.val
+      ? this.search(val, curr.left)
+      : this.search(val, curr.right);
+  }
+
+  // self, left, right
   preOrderTraversal(currentNode = this.root) {
     // Your code here
+    // DO THE THANG
+    // Traverse Left
+    // Traverse Right
   }
 
-
+  // left, self, right
   inOrderTraversal(currentNode = this.root) {
     // Your code here
+    // Traverse Left
+    // DO THE THANG
+    // Traverse Right
   }
 
-
+  // left, right, self
   postOrderTraversal(currentNode = this.root) {
-    // Your code here
+    // Your code here\
+    // Traverse Left
+    // Traverse Right
+    // DO THE THANG
   }
 
     // Breadth First Traversal - Iterative
@@ -76,7 +116,20 @@ class BinarySearchTree {
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
+  }
 }
-}
+
+const bst = new BinarySearchTree();
+
+bst.insert(4);
+bst.insert(2);
+bst.insert(6);
+bst.insert(1);
+bst.insert(3);
+bst.insert(5);
+bst.insert(7);
+
+console.log(bst.search2(3))
+console.log(bst.search2(33))
 
 module.exports = { BinarySearchTree, TreeNode };
