b0f42397349ca24d82e93bf3f71bed7de1332e11
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-08-01 15:43:22 -0500

    inheritance and polymorphism

--- week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
@@ -1,6 +1,28 @@
 const Person = require('./person');
 
 // Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(stood1, stood2) {
+    if (stood1.GPA > stood2.GPA) {
+      return `${stood1.firstName} ${stood1.lastName} has the higher GPA.`
+    } else if (stood2.GPA > stood1.GPA) {
+      return `${stood2.firstName} ${stood2.lastName} has the higher GPA.`
+    } else {
+      return "Both students have the same GPA.";
+    }
+  }
+}
+
+// const shane = new Student('shane', 'wilkey', 'software engineering', 3.0);
+// // shane.age = 41
+// console.log(shane)
+// console.log(shane.introduce())
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
