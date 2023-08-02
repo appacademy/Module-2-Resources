c59b66d4a12a1f89ee4b47e861d88d056ea360f3
Briana Robinson <briana.n.robinson@gmail.com>
2023-08-02 10:19:37 -0700

    context short practice solutions and briana context update

--- week-4/d3-context-bind-call-apply/practices/01-context-regular-functions/context.js
+++ week-4/d3-context-bind-call-apply/practices/01-context-regular-functions/context.js
@@ -16,22 +16,38 @@ const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
 // eat(); // ?
+// Function style invocation - context: global
 
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
 // nemo.eat(); // ?
-
+// Method style invocation - context: nemo instance
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
 // eat(); // ?
-
+// Function style invocation - context: global
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+// nemo.swim(); // Nemo swimming in the water
+// Method style invocation - context: nemo instance
 
 
 /********************************* Scenario 5 *********************************/
 // const swim = nemo.swim;
 // swim(); // ?
+// Function style invocation - context lost/undefined because of class' strict mode
+// TypeError
+
+// Fix
+
+const nemoSwim = nemo.swim.bind(nemo)
+nemoSwim() // Nemo swimming in the water
+
+const briana = {
+  name: "Briana"
+}
+
+const brianaSwim = nemo.swim.bind(briana)
+brianaSwim() // Briana swimming in the water
