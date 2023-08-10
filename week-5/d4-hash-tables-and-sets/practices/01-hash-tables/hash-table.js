fea0c7a8d551e679165a302c03842d4737a9445d
Briana Robinson <briana.n.robinson@gmail.com>
2023-08-10 09:48:38 -0700

    hash-table short practice, constructor, hash, hashMod

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -12,14 +12,36 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0
+    this.capacity = numBuckets
+    this.data = new Array(this.capacity).fill(null)
   }
 
   hash(key) {
     // Your code here
+    // console.log("key:   ", key)
+
+    // // find hash string
+    // let hash = sha256(key)
+    // console.log("sha256 hash of Key:  ", hash)
+
+    // // take first 8 chars
+    // let hash8 = hash.slice(0, 8)
+    // console.log("first 8 chars:   ", hash8)
+
+    // // convert hexa to deci
+    // let deci = parseInt(hash8, 16)
+    // // let deci = parseInt(`0x${hash8}`)
+    // console.log(deci, "\n\n")
+
+    // return deci
+
+    return parseInt(sha256(key).slice(0, 8), 16)
   }
 
   hashMod(key) {
     // Your code here
+    return this.hash(key) % this.capacity
   }
 
   insertNoCollisions(key, value) {
