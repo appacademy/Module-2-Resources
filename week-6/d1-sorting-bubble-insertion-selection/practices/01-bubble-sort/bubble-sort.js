d2a3d1841d1982d830b0f9e60baf297d58e20098
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2023-08-14 16:05:38 -0500

    bubble sort

--- week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/01-bubble-sort/bubble-sort.js
@@ -1,18 +1,50 @@
 
 function bubbleSort(arr) {
 
-  // Iterate through the array
+  let swapped = true;
 
-    // If the current value is greater than its neighbor to the right
-      // Swap those values
+  while (swapped) {
+    swapped = false;
+    // Iterate through the array
+    for (let i = 0; i < arr.length - 1; i++) {
+      // If the current value is greater than its neighbor to the right
+        // Swap those values
+      if (arr[i] > arr[i+1]) {
+        // let temp = arr[i];
+        // arr[i] = arr[i+1];
+        // arr[i+1] = temp;
+        [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+        swapped = true;
+        // Do not move this console.log
+        console.log(arr.join(","));
+      }
+    }
+    // If you get to the end of the array and no swaps have occurred, return
+    // if (!swapped) return arr;
+    // Otherwise, repeat from the beginning
 
-      // Do not move this console.log
-      console.log(arr.join(","));
+  }
 
-  // If you get to the end of the array and no swaps have occurred, return
-
-  // Otherwise, repeat from the beginning
 
 }
+
+const arr = [2,4,6,8,1,3,5,7,9];
+
+function bubbleSort2(arr) {
+  let swapped = false;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+        swapped = true;
+        console.log(arr.join(','));
+      }
+    }
+    if (!swapped) return arr;
+  }
+}
+
+bubbleSort2(arr);
 
 module.exports = bubbleSort;
