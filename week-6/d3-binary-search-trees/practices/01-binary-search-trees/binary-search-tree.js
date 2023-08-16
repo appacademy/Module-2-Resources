d394bbcf55f48614ca120b5570b560464efd73c6
Briana Robinson <briana.n.robinson@gmail.com>
2023-08-16 10:36:00 -0700

    bst added little notes for next practice

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -33,23 +33,72 @@ class BinarySearchTree {
     return this
   }
 
+  /*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+
+  */
   search(val) {
     // Your code here
+    // iterative
+    let currNode = this.root
+
+    while (currNode) {
+      if (currNode.val === val) return true
+      if (val < currNode.val) currNode = currNode.left
+      else currNode = currNode.right
+    }
+
+    return false
+  }
+
+  search2(val, currNode = this.root) {
+    // recursive
+    if (!currNode) return false
+    if (currNode.val === val) return true
+
+    if (val < currNode.val) return this.search2(val, currNode.left)
+    else return this.search2(val, currNode.right)
   }
 
 
+  /*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+
+  */
+
+  // self, left, right
   preOrderTraversal(currentNode = this.root) {
     // Your code here
+
+    // Do the thing with currentNode
+    // left subtree
+    // right subtree
   }
 
 
+  // left, self, right
   inOrderTraversal(currentNode = this.root) {
     // Your code here
+
+    // left subtree
+    // Do the thing with currentNode
+    // right subtree
   }
 
-
+// left, right, self
   postOrderTraversal(currentNode = this.root) {
     // Your code here
+    // left subtree
+    // right subtree
+    // Do the thing with currentNode
   }
 
     // Breadth First Traversal - Iterative
@@ -64,3 +113,19 @@ class BinarySearchTree {
 }
 
 module.exports = { BinarySearchTree, TreeNode };
+
+
+bst = new BinarySearchTree();
+bst.insert(4);
+bst.insert(2);
+bst.insert(6);
+bst.insert(1);
+bst.insert(3);
+bst.insert(5);
+bst.insert(7);
+
+console.log(
+  bst.search2(1), // true
+  bst.search2(7), // true
+  bst.search2(8) // false
+)
