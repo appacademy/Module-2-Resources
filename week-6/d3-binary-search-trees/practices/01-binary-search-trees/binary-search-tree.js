f0a255268467e93d2671aa6e8e78a3a8ec40a25c
Briana Robinson <briana.n.robinson@gmail.com>
2023-08-16 09:36:10 -0700

    bst short practice 1

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -11,10 +11,26 @@ class BinarySearchTree {
 
   constructor() {
     // Your code here
+    this.root = null
   }
 
   insert(val, currentNode=this.root) {
     // Your code here
+    const newNode = new TreeNode(val)
+    if (!this.root) {
+      this.root = newNode
+      return this
+    }
+
+    if (val < currentNode.val) {
+      if (!currentNode.left) currentNode.left = newNode
+      else this.insert(val, currentNode.left)
+    } else {
+      if (!currentNode.right) currentNode.right = newNode
+      else this.insert(val, currentNode.right)
+    }
+
+    return this
   }
 
   search(val) {
