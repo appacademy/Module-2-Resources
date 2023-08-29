e00bf2b4985ca6505476ca59adc97ef068d6fa70
Briana Robinson <briana.n.robinson@gmail.com>
2023-08-29 13:17:17 -0700

    briana inheritance

--- week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
+++ week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
@@ -1,5 +1,37 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
 
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople (peopleArr) {
+    // console.log(peopleArr, " ===> peopleArr")
+    // console.log(Array.isArray(peopleArr))
+    if (!Array.isArray(peopleArr)) {
+      return console.log("introducePeople only takes an array as an argument.")
+
+    }
+
+    for (let person of peopleArr) {
+      if (!(person instanceof Person)) {
+        console.log("All items in array must be Person class instances.")
+        return
+      }
+    }
+
+    peopleArr.forEach(person => {
+      person.introduce()
+
+    })
+
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
