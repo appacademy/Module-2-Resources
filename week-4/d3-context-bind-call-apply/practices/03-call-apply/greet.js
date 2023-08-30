cafca03b395fe074d2074430944724c74d081c77
Briana Robinson <briana.n.robinson@gmail.com>
2023-08-30 14:06:32 -0700

    fat arrow context, 3rd short practice

--- week-4/d3-context-bind-call-apply/practices/03-call-apply/greet.js
+++ week-4/d3-context-bind-call-apply/practices/03-call-apply/greet.js
@@ -1,10 +1,11 @@
 // DO NOT MODIFY THE say FUNCTION
 function say(name) {
+  // console.log(this)
   console.log(`${this.message} ${name}!`);
 }
 
 helloMessage = { message: 'Hello,' };
 heyThereMessage = { message: 'Hey there,' };
 
-say(helloMessage, "John");
-say(heyThereMessage, "Michael");
+say.call(helloMessage, "John"); // Hello, John!
+say.call(heyThereMessage, "Michael"); // Hey there, Michael!
