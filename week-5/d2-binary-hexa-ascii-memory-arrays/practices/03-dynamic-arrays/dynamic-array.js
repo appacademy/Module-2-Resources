f1ad5a3195e8915d936f5acd7a0246bc78de094b
Briana Robinson <briana.n.robinson@gmail.com>
2023-09-06 13:09:59 -0700

    dynamic array short practice

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
@@ -3,16 +3,40 @@ class DynamicArray {
   constructor(defaultSize=4) {
 
     // Your code here
+    this.length = 0
+    this.capacity = defaultSize
+    this.data = new Array(this.capacity)
+
+    // console.log(this.data)
   }
 
   read(index) {
 
     // Your code here
+    // console.log(this.data)
+    return this.data[index]
   }
 
-  unshift(val) {
+  unshift(val) { // O(n) Time
 
     // Your code here
+
+    console.log("\n\n BEGINNING", this.data)
+
+    // move/shift original values to the right of array
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1]
+      console.log("IN FOR LOOP", this.data)
+
+    }
+
+    // add new value at front of array
+    this.data[0] = val
+
+    this.length++
+    console.log("END", this.data)
+
+    return this.length
   }
 
 }
