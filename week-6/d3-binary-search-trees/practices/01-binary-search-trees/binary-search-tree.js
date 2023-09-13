5816d65d2e5444f8474ac2faf5e6de2bb3ea09d4
southwestmogrown <southwestmogrown@gmail.com>
2023-09-13 12:17:18 -0500

    bst search

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -11,16 +11,80 @@ class BinarySearchTree {
 
   constructor() {
     // Your code here
+    this.root = null;
   }
 
   insert(val, currentNode=this.root) {
     // Your code here
+    const node = new TreeNode(val);
+
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+
+    if (val < currentNode.val) {
+      if (!currentNode.left) {
+        currentNode.left = node;
+        return this;
+      } else {
+        return this.insert(val, currentNode.left);
+      }
+    }
+
+    if (val > currentNode.val) {
+      if (!currentNode.right) {
+        currentNode.right = node;
+        return this;
+      } else {
+        return this.insert(val, currentNode.right);
+      }
+    }
   }
 
-  search(val) {
+/*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+*/
+
+  // search(val) {
+  //   // Your code here
+  //   let curr = this.root;
+    
+  //   while (curr) {
+  //     if (curr.val === val) return true;
+
+  //     // if (val < curr.val) {
+  //     //   curr = curr.left;
+  //     // } else {
+  //     //   curr = curr.right;
+  //     // }
+  //     curr = val < curr.val ? curr.left : curr.right;
+
+  //   }
+
+  //   return false;
+  // }
+  search(val, curr=this.root) {
     // Your code here
-  }
+    // if (!curr) return false;
+    // if (curr.val === val) return true;
 
+    // if (val < curr.val) {
+    //   return this.search(val, curr.left);
+    // } else {
+    //   return this.search(val, curr.right);
+    // }
+    return !curr 
+      ? false 
+      : curr.val === val 
+      ? true : val < curr.val 
+      ? this.search(val, curr.left) 
+      : this.search(val, curr.right);
+  }
 
   preOrderTraversal(currentNode = this.root) {
     // Your code here
