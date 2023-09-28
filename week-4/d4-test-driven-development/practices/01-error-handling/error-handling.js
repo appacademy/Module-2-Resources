79e310721decab85dad8e573284a44f983afbf52
southwestmogrown <southwestmogrown@gmail.com>
2023-09-28 18:29:46 -0500

    w4d4

--- week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
+++ week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
@@ -1,26 +1,53 @@
 // 1.
-function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
+// function sum(array) {
+//   let sum = 0;
+//   try {
+//     for (let i = 0; i < array.length; i++) {
+//       sum += array[i];
+//     }
+//   } catch (e) {
+//     if (e instanceof TypeError) {
+//       console.error(e.message);
+//     }
+//   }
+//   return sum;
+// }
 
-let res = sum(null);
-console.log(res);
+// let res = sum([]);
+// console.log(res);
 
-// 2.
-// tests
-sayName("Alex");
-sayName(1);
+// // 2.
+// // tests
+// function sayName(name) {
+//   if (typeof name !== 'string') {
+//     throw new TypeError("Invalid name! Must be a string!");
+//   }
+
+//   console.log(name)
+// }
+
+// try {
+//   sayName("Alex");
+//   sayName(1);
+//   console.log('will not get here')
+// } catch (e) {
+//   console.error(e.message);
+// }
+
 // Your code here
 
-// 3.
+// // 3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
 
   console.log(greeting);
+}
+
+try {
+  greet('Howdy');
+  greet(undefined)
+} catch {
+  console.log('Hello World')
 }
