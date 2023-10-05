d06fb34ebf3c061bffd61948ed470d13436fa7ea
southwestmogrown <southwestmogrown@gmail.com>
2023-10-05 11:29:48 -0500

    hash hashMod methods

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -12,14 +12,24 @@ class HashTable {
 
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
     // Your code here
+    // let hash = sha256(key);
+    // hash = hash.slice(0,8);
+    // hash = parseInt(hash, 16);
+    // // console.log({ hash })
+    // return hash;
+    return parseInt(sha256(key).slice(0,8), 16);
   }
 
   hashMod(key) {
     // Your code here
+    return this.hash(key) % this.capacity /* || this.data.length */;
   }
 
   insertNoCollisions(key, value) {
@@ -35,6 +45,11 @@ class HashTable {
   }
 
 }
+
+
+// const hash = sha256('shane');
+
+// console.log(parseInt(hash.slice(0, 8), 16) % 4);
 
 
 module.exports = HashTable;
