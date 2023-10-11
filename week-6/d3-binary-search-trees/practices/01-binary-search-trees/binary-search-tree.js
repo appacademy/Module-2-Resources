896fa1616af7e73159c26d9cdad5b855477deedf
southwestmogrown <southwestmogrown@gmail.com>
2023-10-11 11:59:15 -0500

    insert search BST

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -11,14 +11,91 @@ class BinarySearchTree {
 
   constructor() {
     // Your code here
+    this.root = null;
+
+   
   }
+
+/*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+
+*/
 
   insert(val, currentNode=this.root) {
     // Your code here
+    const newNode = new TreeNode(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } 
+
+    if (val < currentNode.val) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+        return this;
+      } else {
+        return this.insert(val, currentNode.left);
+      }
+    } 
+
+    if (val > currentNode.val) {
+      if (!currentNode.right) {
+        currentNode.right = newNode;
+        return this;
+      } else {
+        return this.insert(val, currentNode.right);
+      }
+    }
   }
 
-  search(val) {
-    // Your code here
+/*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+
+*/
+
+  // search(val) {
+  //   // Your code here
+  //   let curr = this.root;
+
+  //   while (curr) {
+  //     if (curr.val === val) return true;
+
+  //     // if (val < curr.val) {
+  //     //   curr = curr.left;
+  //     // } else {
+  //     //   curr = curr.right;
+  //     // }
+  //     curr = val < curr.val ? curr.left : curr.right;
+
+  //   }
+  //   return false
+  // }
+
+  search(val, curr=this.root) {
+    // if (!curr) return false;
+    // if (curr.val === val) return true;
+
+    // if (val < curr.val) {
+    //   return this.search(val, curr.left);
+    // } else {
+    //   return this.search(val, curr.right);
+    // }
+    return !curr 
+      ? false 
+      : curr.val === val 
+      ? true 
+      : val < curr.val 
+      ? this.search(val, curr.left) 
+      : this.search(val, curr.right); 
   }
 
 
