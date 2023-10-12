b46a5ca9c94d8e68d385e79520795e84efb56f84
Briana Robinson <briana.n.robinson@gmail.com>
2023-10-12 13:50:27 -0400

    shortest path

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
@@ -9,6 +9,31 @@ const adjList = {
 
 function aShortestPath(start, end) {
   // Your code here
+  const queue = [[start]]  // contain paths
+  const visited = new Set().add(start)
+
+  while (queue.length) {
+    // console.log({queue})
+    // console.log({visited})
+    const currPath = queue.shift() // [1]
+    const lastVal = currPath[currPath.length - 1] // value at end of path
+
+    // Do the thing
+    if (lastVal === end) return currPath
+
+    // Find neighbors
+    const neighbors = adjList[lastVal]
+
+    neighbors.forEach(neighbor => {
+      if (!visited.has(neighbor)) {
+        queue.push([...currPath, neighbor])
+        visited.add(neighbor)
+      }
+    })
+
+  }
+
+  return false
 }
 
 console.log("First Test:");
