bc524178cc08435e37c8ca4c50b0f53cb27bf4cf
Briana Robinson <briana.n.robinson@gmail.com>
2023-10-24 14:11:17 -0400

    inheritance

--- week-4/d2-es6-classes/notes/briana-notes/class.js
+++ week-4/d2-es6-classes/notes/briana-notes/class.js
@@ -36,4 +36,39 @@ const song3 = new Song("Mantra", "Makaya McCraven", 2018)
 // song1.playSong() // Playing The List!
 // song2.playSong() // Playing Viva La Vida!
 
-console.log(Song.putSongsInPlaylist(song1, song3, song2)) // [ 'The List', 'Mantra', 'Viva La Vida' ]
+// console.log(Song.putSongsInPlaylist(song1, song3, song2)) // [ 'The List', 'Mantra', 'Viva La Vida' ]
+
+
+// console.log(song1 instanceof Song) // true
+
+
+
+// Inheritance - pass down all properties and methods from parent class to child class
+// child class may have extra properties and/or methods
+
+// extends will reference parent class
+class ClassicalSong extends Song {
+
+    // without constructor, this child class will use parent's constructor
+    constructor (name, artist, releaseYear, period) {
+        // if you have a constructor in child class, always call super
+        super(name, artist, releaseYear)
+        this.period = period
+    }
+
+}
+
+const classical1 = new ClassicalSong("Prelude in C# Minor", "Sergei Rachmaninov", 1892, "Romantic")
+console.log(classical1)
+/*
+ClassicalSong {
+  name: 'Prelude in C# Minor',
+  artist: 'Sergei Rachmaninov',
+  releaseYear: 1892,
+  period: 'Romantic'
+}
+*/
+
+classical1.playSong() // Playing Prelude in C# Minor!
+console.log(classical1 instanceof ClassicalSong) // true
+console.log(classical1 instanceof Song) // true
