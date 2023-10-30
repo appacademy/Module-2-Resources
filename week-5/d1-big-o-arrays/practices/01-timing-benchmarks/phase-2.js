886ea8f9aa7db35d99713b9f9a7bef4afb3c0cd6
Briana Robinson <briana.n.robinson@gmail.com>
2023-10-30 19:07:31 -0400

    phase 2 and 3

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
@@ -4,14 +4,33 @@ const [addNums, addManyNums] = require("./phase-1");
 function addNums10(increment) {
   // Fill this in
 
+  const arr = []
 
+  for (let i = 1; i <= 10; i++) {
+    arr.push(addNums(i * increment))
+  }
+
+  return arr
 }
+// Time O(1 + (10 * n) + 1) => O(1 + n + 1)
+// => O(n)
+// Space O(1), making a new array but it will always contain 10 items
+
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
+  const arr = []
 
+  for (let i = 1; i <= 10; i++) {
+    //       O(n^2)
+    arr.push(addManyNums(i * increment))
+  }
 
+  return arr
 }
+
+// Time O(n^2)
+// Space O(1)
 
 module.exports = [addNums10, addManyNums10];
