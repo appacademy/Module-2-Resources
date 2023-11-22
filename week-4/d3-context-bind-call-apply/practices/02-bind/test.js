7a1a1b05af6fa8dff3f57b20d6c91ae0aba27cef
southwestmogrown <southwestmogrown@gmail.com>
2023-11-22 12:01:01 -0600

    bind practice

--- week-4/d3-context-bind-call-apply/practices/02-bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/test.js
@@ -1,1 +1,23 @@
 // Your code here
+const Employee = require('./employee');
+
+const john = new Employee('John Wick', 'Dog Lover');
+        // => sayNameFunc()
+setTimeout(john.sayName.bind(john), 200)
+
+// setTimeout(() => {
+//     john.sayName();
+// }, 2000);
+
+setTimeout(john.sayOccupation.bind(john), 300);
+
+// function adder(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+// const adderFuncBound = adder.bind({}, 6843473, 549827)
+
+// adderFuncBound();
+// adderFuncBound();
+// adderFuncBound();
+// adderFuncBound();
