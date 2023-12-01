bb8a5a5357f3c36227fbcfafe9460ef671224860
southwestmogrown <southwestmogrown@gmail.com>
2023-12-01 12:29:35 -0600

    hash tables insert w/ collisions

--- week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
+++ week-5/d4-hash-tables-and-sets/practices/01-hash-tables/hash-table.js
@@ -1,4 +1,4 @@
-const sha256 = require('js-sha256');
+const sha256 = require("js-sha256");
 
 class KeyValuePair {
   constructor(key, value) {
@@ -9,32 +9,74 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
     // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
     // Your code here
+    // console.log({ key });
+    // let hash = sha256(key);
+    // hash = hash.substring(0, 8);
+    // hash = parseInt(hash, 16);
+    // // console.log({ hash });
+    // return hash;
+
+    return parseInt(sha256(key).substring(0, 8), 16);
   }
 
   hashMod(key) {
     // Your code here
+    // const hash = this.hash(key);
+    // // console.log({ hash });
+    // return hash % this.capacity;
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
     // Your code here
+    const newKvp = new KeyValuePair(key, value);
+    // console.log({ newKvp });
+    const index = this.hashMod(key);
+
+    if (this.data[index])
+      throw new Error("hash collision or same key/value pair already exists!");
+
+    this.data[index] = newKvp;
+    // console.log(this.data);
+    this.count++;
   }
 
   insertWithHashCollisions(key, value) {
     // Your code here
+    const kvp = new KeyValuePair(key, value);
+    const index = this.hashMod(key);
+
+    // if (this.data[index]){}
+    kvp.next = this.data[index]; // could be null
+    this.data[index] = kvp;
+    this.count++;
+    return this.count;
   }
 
   insert(key, value) {
     // Your code here
-  }
+    const kvp = new KeyValuePair(key, value);
+    const index = this.hashMod(key);
 
+    kvp.next = this.data[index]; // could be null
+    this.data[index] = kvp;
+    this.count++;
+    return this.count;
+  }
 }
 
+// const hash = sha256("ab");
+// // const hash2 = sha256("A");
+// console.log(parseInt(hash.substring(0, 8), 16) % 4);
+// // console.log(hash2);
 
 module.exports = HashTable;
