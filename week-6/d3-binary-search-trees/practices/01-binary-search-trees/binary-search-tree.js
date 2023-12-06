70889ebfa8ef399e30ba3a58ac19348b5b2d4901
southwestmogrown <southwestmogrown@gmail.com>
2023-12-06 11:13:50 -0600

    bst insert and search

--- week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
+++ week-6/d3-binary-search-trees/practices/01-binary-search-trees/binary-search-tree.js
@@ -8,35 +8,77 @@ class TreeNode {
 }
 
 class BinarySearchTree {
-
   constructor() {
     // Your code here
+    this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
+  /*
+              4
+           /     \
+          2       6
+        /   \   /   \
+       1     3 5     7
+*/
+
+  insert(val, currentNode = this.root) {
     // Your code here
+    const node = new TreeNode(val);
+
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+
+    if (val < currentNode.val) {
+      if (!currentNode.left) {
+        currentNode.left = node;
+        return this;
+      } else {
+        return this.insert(val, currentNode.left);
+      }
+    }
+
+    if (val > currentNode.val) {
+      if (!currentNode.right) {
+        currentNode.right = node;
+        return this;
+      } else {
+        return this.insert(val, currentNode.right);
+      }
+    }
   }
 
   search(val) {
     // Your code here
-  }
+    let curr = this.root;
 
+    while (curr) {
+      if (curr.val === val) return true;
+      curr = val < curr.val ? curr.left : curr.right;
+
+      // if (val < curr.val) {
+      //   curr = curr.left;
+      // } else {
+      //   curr = curr.right;
+      // }
+    }
+    return false;
+  }
 
   preOrderTraversal(currentNode = this.root) {
     // Your code here
   }
 
-
   inOrderTraversal(currentNode = this.root) {
     // Your code here
   }
-
 
   postOrderTraversal(currentNode = this.root) {
     // Your code here
   }
 
-    // Breadth First Traversal - Iterative
+  // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
     // your code here
   }
@@ -44,7 +86,7 @@ class BinarySearchTree {
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
-}
+  }
 }
 
 module.exports = { BinarySearchTree, TreeNode };
