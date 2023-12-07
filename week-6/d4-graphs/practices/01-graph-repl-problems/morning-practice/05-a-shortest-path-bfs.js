873743531c2fdbafe458045b56d8a3ed38f01152
southwestmogrown <southwestmogrown@gmail.com>
2023-12-07 12:20:57 -0600

    shortest path degress of separation

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/05-a-shortest-path-bfs.js
@@ -4,11 +4,29 @@ const adjList = {
   3: [2, 4],
   4: [3, 5],
   5: [1, 2, 4],
-  6: []
-}
+  6: [],
+};
 
 function aShortestPath(start, end) {
   // Your code here
+  const queue = [[start]];
+  const visited = new Set([start]);
+
+  while (queue.length) {
+    const currPath = queue.shift();
+    const currNode = currPath[currPath.length - 1];
+
+    // DO THE THING
+    if (currNode === end) return currPath;
+
+    adjList[currNode].forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        queue.push([...currPath, neighbor]);
+        visited.add(neighbor);
+      }
+    });
+  }
+  return false;
 }
 
 console.log("First Test:");
@@ -17,3 +35,15 @@ console.log("Second Test:");
 console.log(aShortestPath(4, 1)); // -> [ 4, 5, 1 ] (One possible solution)
 console.log("Third Test:");
 console.log(aShortestPath(6, 1)); // -> false
+
+/*
+queue = [[1]]
+currPath = [1]
+currNode = 1
+
+queue = [ [1,2,5], [1,5,4]]
+currPath = [1,2,3]
+currNode = arr[arr.length-1] => 3
+
+queue.push([...currPath, neighbor])
+*/
