195ef47de8c45ee5155864d43845b387a5c33f29
southwestmogrown <southwestmogrown@gmail.com>
2023-12-07 11:06:46 -0600

    refactor BFT

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/03-refactor-bft.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/03-refactor-bft.js
@@ -1,22 +1,42 @@
 const adjList = {
-    1: [2, 5],
-    2: [1, 3, 5],
-    3: [2, 4],
-    4: [3, 5, 6],
-    5: [1, 2, 4],
-    6: [4]
-}
+  1: [2, 5],
+  2: [1, 3, 5],
+  3: [2, 4],
+  4: [3, 5, 6],
+  5: [1, 2, 4],
+  6: [4],
+};
 
 function printBreadthFirst(start) {
-    // Paste your previous code here and refactor it
+  // Paste your previous code here and refactor it
+  const queue = [start];
+  const visited = new Set([start]);
+
+  const res = [];
+
+  while (queue.length) {
+    const curr = queue.shift();
+
+    // DO THE THING
+    res.push(curr);
+
+    adjList[curr].forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        queue.push(neighbor);
+        visited.add(neighbor);
+      }
+    });
+  }
+  console.log(res);
+  return res;
 }
 
-console.log("First Test:")
+console.log("First Test:");
 printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
-                    // One possible solution:  [ 3, 2, 4, 1, 5, 6 ]
-console.log("Second Test:")
+// One possible solution:  [ 3, 2, 4, 1, 5, 6 ]
+console.log("Second Test:");
 printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-                    // One possible solution:  [ 6, 4, 3, 5, 2, 1 ]
-console.log("Third Test:")
+// One possible solution:  [ 6, 4, 3, 5, 2, 1 ]
+console.log("Third Test:");
 printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
-                        // One possible solution:  [ 4, 3, 5, 6, 2, 1 ]
+// One possible solution:  [ 4, 3, 5, 6, 2, 1 ]
