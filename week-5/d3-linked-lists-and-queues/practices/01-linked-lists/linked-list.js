b13cd924879a30975bfd658d2fe4473873bdb1e0
Briana Robinson <briana.n.robinson@gmail.com>
2024-01-04 12:28:23 -0500

    singly LL short practice

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/linked-list.js
@@ -8,14 +8,47 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null
+    this.length = 0
   }
 
   addToHead(val) {
     // Your code here
+    const newNode = new LinkedListNode(val)
+
+    if (!this.head) {
+      this.head = newNode
+    } else {
+      newNode.next = this.head
+      this.head = newNode
+    }
+
+    // this.print()
+    this.length++
+    return this.length
   }
 
   addToTail(val) {
     // Your code here
+    const newNode = new LinkedListNode(val)
+
+    if (!this.head) {
+      this.head = newNode
+    } else {
+      let curr = this.head
+
+      while (curr.next) {
+        curr = curr.next
+      }
+
+      // after while loop, curr.next should already be null
+      // we've reached the end of the LL, so curr's next should now point to new node
+      curr.next = newNode
+    }
+
+    // this.print()
+    this.length++
+    return this.length
   }
 
   // You can use this function to help debug
