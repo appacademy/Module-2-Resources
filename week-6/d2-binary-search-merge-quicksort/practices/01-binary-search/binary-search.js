390c2ba3c76ad17911e1efb0841d238314af9a1b
southwestmogrown <southwestmogrown@gmail.com>
2024-01-09 12:44:54 -0600

    merge sort

--- week-6/d2-binary-search-merge-quicksort/practices/01-binary-search/binary-search.js
+++ week-6/d2-binary-search-merge-quicksort/practices/01-binary-search/binary-search.js
@@ -1,29 +1,41 @@
-function linearSearch (arr, target) {
+function linearSearch(arr, target) {
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] === target) return i;
+  // }
+  // return -1;
 
   // Can you solve this in one line?
-
-};
-
-function binarySearch(arr, target) {
-
-  // Set integers pointing to the high and low range of possible indices
-
-  // While high and low indices do not overlap...
-
-    // Find the midpoint between high and low indices
-
-    // Compare the target value to the midpoint value
-
-    // If the target equals the midpoint...
-      // Return the midpoint index
-    // If the target is higher than the midpoint...
-      // Move the low pointer to midpoint + 1
-    // If the target is less than the midpoint...
-      // Move the high pointer to midpoint - 1
-
-  // Return -1 if the loop exits with overlapping pointers
-
+  return arr.indexOf(target);
 }
 
+function binarySearch(arr, target) {
+  // [1,2,3,4,5,6,7,8] => 7
+  // Set integers pointing to the high and low range of possible indices
+  let lo = 0;
+  let hi = arr.length - 1;
+  // While high and low indices do not overlap...
+  //             lm h
+  while (lo <= hi) {
+    // [1,2,3,4,5,6,7,8]
+    // Find the midpoint between high and low indices
+    const mid = Math.floor((lo + hi) / 2);
+    // Compare the target value to the midpoint value
+    // If the target equals the midpoint...
+    if (arr[mid] === target) {
+      // Return the midpoint index
+      return mid;
+    } else if (target > arr[mid]) {
+      // If the target is higher than the midpoint...
+      // Move the low pointer to midpoint + 1
+      lo = mid + 1;
+    } else {
+      // If the target is less than the midpoint...
+      // Move the high pointer to midpoint - 1
+      hi = mid - 1;
+    }
+  }
+  // Return -1 if the loop exits with overlapping pointers
+  return -1;
+}
 
-module.exports = [linearSearch, binarySearch]
+module.exports = [linearSearch, binarySearch];
