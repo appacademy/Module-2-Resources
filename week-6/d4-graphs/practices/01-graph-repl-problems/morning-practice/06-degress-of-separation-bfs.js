91ab52cb2c27bb026f89d9f884cad7860ccbd71c
southwestmogrown <southwestmogrown@gmail.com>
2024-01-11 13:02:29 -0600

    matrix lecture

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/06-degress-of-separation-bfs.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/06-degress-of-separation-bfs.js
@@ -4,11 +4,27 @@ const adjList = {
   3: [2, 4],
   4: [3, 5],
   5: [1, 2, 4],
-  6: []
-}
+  6: [],
+};
 
 function degreesOfSeparation(start, end) {
-  // Your code here
+  const queue = [[start]];
+  const visited = new Set([start]);
+
+  while (queue.length) {
+    const currPath = queue.shift();
+    const currNode = currPath[currPath.length - 1];
+
+    if (currNode === end) return currPath.length - 1;
+
+    adjList[currNode].forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        queue.push([...currPath, neighbor]);
+        visited.add(neighbor);
+      }
+    });
+  }
+  return false;
 }
 
 console.log("First Test:");
