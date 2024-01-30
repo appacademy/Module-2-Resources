b6ea46305496f1df6c28017febc87e46a21c97c3
brandon <brandonlaursen95@yahoo.com>
2024-01-30 13:57:03 -0500

    02 practice

--- week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
+++ week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
@@ -1,4 +1,45 @@
-// Your code here
+
+
+
+
+
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  };
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  };
+
+  static introducePeople(people){
+
+    if(!(Array.isArray(people))) {
+      console.log("introducePeople only takes an array as an argument.");
+      return;
+    };
+
+    for(let person of people) {
+      if(!(person instanceof Person)) {
+        console.log("All items in array must be Person class instances.");
+        return;
+      };
+    };
+
+    people.forEach(person => person.introduce())
+  };
+}
+
+
+// const Brandon = new Person('brandon', 'laursen', '28');
+// const Clone = new Person('Clone', 'laursen', '28');
+// // console.log(Brandon.introduce())
+// Person.introducePeople([Brandon, Clone])
+// console.log(Brandon);
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
