dfbcd023abdcdc20179a8b8996be948eb708fbc4
brandon <brandonlaursen95@yahoo.com>
2024-01-30 15:49:10 -0500

    classes

--- week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
+++ week-4/d2-es6-classes/practices/02-basic-class-syntax/classes/person.js
@@ -1,45 +1,38 @@
-
-
-
-
-
 class Person {
   constructor(firstName, lastName, age) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
-  };
+  }
 
   introduce() {
-    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
-  };
+    console.log(
+      `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`
+    );
+  }
 
-  static introducePeople(people){
-
-    if(!(Array.isArray(people))) {
+  static introducePeople(people) {
+    if (!Array.isArray(people)) {
       console.log("introducePeople only takes an array as an argument.");
       return;
-    };
+    }
 
-    for(let person of people) {
-      if(!(person instanceof Person)) {
+    for (let person of people) {
+      if (!(person instanceof Person)) {
         console.log("All items in array must be Person class instances.");
         return;
-      };
-    };
+      }
+    }
 
-    people.forEach(person => person.introduce())
-  };
+    people.forEach((person) => person.introduce());
+  }
 }
-
 
 // const Brandon = new Person('brandon', 'laursen', '28');
 // const Clone = new Person('Clone', 'laursen', '28');
 // // console.log(Brandon.introduce())
 // Person.introducePeople([Brandon, Clone])
 // console.log(Brandon);
-
-
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
