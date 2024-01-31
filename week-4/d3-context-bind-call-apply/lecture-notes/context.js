07fbf082cc14d424fc71b6d7f2717ed20d8a52d7
brandon <brandonlaursen95@yahoo.com>
2024-01-31 11:51:36 -0500

    context

--- week-4/d3-context-bind-call-apply/lecture-notes/context.js
+++ week-4/d3-context-bind-call-apply/lecture-notes/context.js
@@ -66,7 +66,7 @@ const elephant2 = {
 
 
 let sayAgeFunc = elephant2.sayAge;
-console.log(sayAgeFunc());// my name is undefined and i am undefined
+// console.log(sayAgeFunc());// my name is undefined and i am undefined
 
 /*
 
@@ -77,4 +77,171 @@ console.log(sayAgeFunc());// my name is undefined and i am undefined
   * before we called the function on the elephant object
   * Now we are calling the function upon the global object
   * in the global object we haven't defined a name or age variable
+
 */
+
+
+// HOW TO ENSURE OUR CONTEXT NEVER CHANGES
+// * use Bind!
+const elephant3 = {
+  name:'funkfreed',
+  age: 1,
+  whatIsThis: function () {
+		console.log(this);
+	},
+	sayAge: function () {
+		console.log(`my name is ${this.name} and i am ${this.age}`);
+	}
+};
+
+// bind
+// function.bind(context)
+let boundSayAgeFunc = elephant3.sayAge.bind(elephant3)
+// boundSayAgeFunc();// my name is funkfreed and i am 1
+
+
+/*
+  What did we do here?
+  * We are binding context of the sayAge function to the elephant3 object
+  * Now when its called; this refers to elephant3 as opposed to the global object
+  * note the use of the bind
+  * we are keying into elephant3.sayAge function and binding it to the elephant3 object
+  * Syntax
+    * Function.bind(context)
+    * function = elephant3.sayAge
+    * context = elephant3
+  * This now refers to elephant3
+  * boundSayAgeFunc will now always reference elephant3
+  * bind returns a function
+*/
+
+
+
+// BINDING MULTIPLE CONTEXTS TO CREATE MULTIPLE FUNCS
+const elephant4 = {
+	name: 'funkfreed',
+	age: 1
+}
+
+const cat = {
+	name: 'mochi',
+	age: 2
+}
+
+const whale = {
+  name:'laboon',
+  sayHello: function(){
+    console.log(`hello im ${this.name}`);
+  }
+};
+
+// console.log('==>',whale.sayHello)
+// let func = whale.sayHello;
+// console.log(func)
+
+function sayAge() {
+	console.log(`my name is ${this.name} and i am ${this.age}`);
+}
+
+
+// sayAge();
+// Function.bind(context)
+let elephantBoundFunc = sayAge.bind(elephant4)
+// elephantBoundFunc();// my name is funkfreed and i am 1
+
+let catBoundFunc = sayAge.bind(cat);
+// catBoundFunc();// my name is mochi and i am 2
+
+/*
+  We have bound a function to different objects
+  * elephantBoundFunc holds the sayAge function bound to elephant4
+    * this.name would refer to funkfreed
+  * catBoundFunc holds the sayAge function bound to cat
+    * this.name would refer to mochi
+
+  Common Misconceptions
+  * scope: availability of variables at a line in your app
+  * context: the value of this
+*/
+
+
+
+// CAN BIND FUNCS TO WHATEVER OBJECT WE LIKE
+const dragon = {
+  name: "Smaug",
+  sayName: function () {
+    console.log("Hello my name is " + this.name);
+  }
+};
+
+const orc = {
+  name:'Azog',
+}
+
+// * function.bind(context);
+
+// dragon.sayName();// Hello my name is Smaug
+
+let extractedFunc= dragon.sayName;
+// console.log(extractedFunc);
+// extractedFunc();
+
+let dragonBoundFunc = dragon.sayName.bind(dragon)
+// dragonBoundFunc();// Hello my name is Smaug
+
+let orcBoundFunc = dragon.sayName.bind(orc);
+// orcBoundFunc();// Hello my name is Azog
+
+let orcBoundFunc2 = extractedFunc.bind(orc);
+// orcBoundFunc2();// Hello my name is Azog
+
+
+
+// CONTEXT WITH CLASSES
+class Cat {
+  constructor(name, age, breed) {
+      this.name = name;
+      this.age = age;
+      this.breed = breed;
+
+  }
+  print() {
+      console.log(this)
+  }
+
+  // ! Talk about when going over arrow functions
+  // fatArrowPrint = () => console.log(this)
+
+  static printCats(cats) {
+      return cats.forEach(cat => {
+          console.log(cat)
+      })
+  }
+
+}
+
+let obj = {
+  name: 'brandon',
+  myfunc: function(){
+    console.log('hello')
+  }
+};
+
+let brandon = obj.name;
+let func = obj.myfunc;
+
+// console.log(brandon);// brandon
+// // console.log(func());// [Function: myfunc]
+
+let newCat = new Cat('pumpkin', 3, 'ragamuffin');
+// newCat.print();// Cat { name: 'pumpkin', age: 3, breed: 'ragamuffin' }
+
+let printCat = newCat.print;
+// console.log(printCat)
+// printCat();
+
+
+
+let bindPrintCat = newCat.print.bind(newCat);
+
+bindPrintCat();// Cat { name: 'pumpkin', age: 3, breed: 'ragamuffin' }
