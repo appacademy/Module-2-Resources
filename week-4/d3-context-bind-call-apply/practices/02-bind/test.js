eca6b530badf16b37c81b505cc6beca63da7e01f
brandon <brandonlaursen95@yahoo.com>
2024-01-31 13:03:10 -0500

    bind practice

--- week-4/d3-context-bind-call-apply/practices/02-bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/test.js
@@ -1,1 +1,19 @@
 // Your code here
+
+const Employee = require('./employee');
+// console.log(Employee);
+
+
+const john = new Employee('John Wick', 'Dog Lover');
+
+// function sayName(){
+//   console.log(`${this} says hello`)
+// };
+
+// setTimeout(sayName, 2000);
+
+let sayNameBound = john.sayName.bind(john);
+let sayOccupationBound = john.sayOccupation.bind(john);
+
+setTimeout(sayNameBound, 2000);//John Wick says hello
+setTimeout(sayOccupationBound, 2000);//John Wick is a Dog Lover
