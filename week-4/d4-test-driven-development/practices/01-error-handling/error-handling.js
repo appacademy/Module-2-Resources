b68af3cfd670ec36f480a6ec53dcba7aeb28df22
brandon <brandonlaursen95@yahoo.com>
2024-02-01 12:56:35 -0500

    practice 01

--- week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
+++ week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
@@ -1,19 +1,55 @@
 // 1.
 function sum(array) {
   let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+
+
+  try {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  } catch (e) {
+    if(e instanceof TypeError) {
+      console.log(e.message);
+    } else {
+      throw e;
+    }
+
   }
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+
+// try {
+//   let res = sum(null);
+//   console.log(res);
+// } catch (e) {
+//   console.log(e.message);
+// }
+
+// let res = sum(null);
+// console.log(res + 12);
+// console.log(sum([1,2,3]))
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+
+function sayName(name){
+  if(typeof name !== 'string') {
+    throw new TypeError('Invalid name! Must be a string!')
+  };
+  console.log(name);
+}
+
+
+try {
+  // sayName("Alex");
+  sayName(1);
+} catch(e) {
+  console.log('Something went wrong', e)
+}
+
+// console.log('do we make it');
+// sayName("Alex");
 // Your code here
 
 // 3.
@@ -23,4 +59,14 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet();
+} catch(e) {
+  console.log(e, "Hello World!");
+  // function sayBye() {
+  //   console.log('bye')
+  // }
+  // sayBye();
 }
