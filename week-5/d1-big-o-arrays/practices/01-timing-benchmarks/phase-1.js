c3e97eeb2b15d8715f686817b84987d8ad0d72d9
brandon <brandonlaursen95@yahoo.com>
2024-02-05 17:46:15 -0500

    time complexity

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-1.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-1.js
@@ -1,14 +1,45 @@
+
+
+
+
 // Adds up positive integers from 1-n
 function addNums(n) {
-  // Fill this in
-}
+  // 1
+  let total = 0;
 
+  // 1 (1 * n) + (1 * n)
+  for(let i = 1; i <= n; i++){
+    // (1 * n)
+    total += i;
+  };
+
+  return total;
+}
+// T(1 + (1 * n) + (1 * n) + (1 * n) + 1)
+// T(1 1n + 1n + 1n + 1)
+// T(2 + 3n)
+// T(n)
+// Time complexity - O(n)
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
-}
 
+  let total = 0;
+
+  // 1 + (1 * n) + (1 * n)
+  for(let i  = 1; i <= n; i++){
+    // O(n)
+    // n amount of work n amount times
+    // (1 * n) + O(n)
+    total += addNums(i);
+  };
+
+  return total;
+}
+// T(n * n);
+// T(n^2)
+//1 + (1 * n) + (1 * n) + (1 * n) * O(n)
+// O(n^2)
 
 
 module.exports = [addNums, addManyNums];
