47d7e78844fa9b392437e93974d9ed0d166f7223
brandon <brandonlaursen95@yahoo.com>
2024-02-05 19:16:57 -0500

    phase 2 + 3

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
@@ -1,16 +1,40 @@
+
+
+
+
+
+
 const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
+// ? Time complexity - O(n)
 function addNums10(increment) {
-  // Fill this in
 
+  let res = [];
 
+  // 10
+  for(let i = 1; i <= 10; i++) {
+    // were performing O(n) operation a 10/constant of times
+    res.push(addNums(i * increment));
+  }
+
+  return res;
 }
 
 // Runs `addManyNums` in 10 increasing increments
+// ? Time complexity - O(n^2)
 function addManyNums10(increment) {
-  // Fill this in
 
+
+  let res = [];
+
+  // 10
+  for(let i = 1; i <= 10; i++) {
+    // were performing O(n^2) operation a 10/constant of times
+    res.push(addManyNums(i * increment));
+  }
+
+  return res;
 
 }
 
--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
@@ -2,12 +2,55 @@ const [addNums, addManyNums] = require("./phase-1");
 
 
 
-n = 1000
-console.log(`addNums(${n}): `);
-addNums10Timing(10000000);
+
+function addNums10Timing(increment) {
+  let res = [];
+
+
+  for (let i = 1; i <= 10; i++) {
+    startTime = Date.now();
+    res.push(addNums(i * increment));
+    endTime = Date.now();
+    console.log(`${endTime - startTime}`);
+  }
+
+  return res;
+}
+
+
+function addManyNums10Timing(increment) {
+  let res = [];
+
+  for (let i = 1; i <= 10; i++) {
+    startTime = Date.now();
+    res.push(addManyNums(i * increment));
+    endTime = Date.now();
+    console.log(`${endTime - startTime}`);
+  }
+
+  return res;
+}
+
+
+n = 1000;
+console.log(`addNums(${n}ms): `);
+
+
+// startTime = Date.now();
+addNums10Timing(50000000);
+// endTime = Date.now();
+
+// console.log(`Time 1: ${endTime - startTime}ms`);
+
 
 console.log("\n***********\n");
 
-n = 1000
-console.log(`addManyNums(${n}): `);
-addManyNums10Timing(1000);
+n = 1000;
+console.log(`addManyNums(${n}ms): `);
+
+
+// startTime = Date.now();
+addManyNums10Timing(9000);
+// endTime = Date.now();
+
+// console.log(`Time 2: ${endTime - startTime}ms`);
