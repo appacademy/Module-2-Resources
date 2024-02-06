a6a3bdbc60bfb53b2b95395d0c31087e829770a5
brandon <brandonlaursen95@yahoo.com>
2024-02-06 16:21:17 -0500

    dynamic arrays

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
@@ -6,19 +6,52 @@ class DynamicArray {
 
     // Your code here
     this.data = new Array(defaultSize);
+    this.length = 0;
+    this.capacity = defaultSize;
+
   }
 
   read(index) {
 
-    // Your code here
+    if(index >= this.length) return undefined;
+
+    return this.data[index];
   }
 
   unshift(val) {
+    // add to the front
+    // [1,2,3,4]
+    // 0 [0,1,2,3,4]
+    // console.log(this.data)
+    for(let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+      // console.log(this.data)
+    }
 
-    // Your code here
+    this.data[0] = val
+    // console.log(this.data)
+    this.length++;
   }
 
 }
 
+// let arr = [1];
+// console.log(arr[1]);// undefined
+
+let myArr = new DynamicArray()
+myArr.unshift(0)
+// length - 0
+// this.data = [<e>,<e>,<e>,<e>,<e>,<e> ,<e>,<e>]
+// this.capacity = 8
+
+// function name(defaultName = 'brandon') {
+//   let greeting = 'hello'
+//   console.log(`${greeting} ${defaultName}`);
+// }
+
+
+// name('maica', 'hello')
+// name('tim', 'hello')
+// name('kim', 'hello')
 
 module.exports = DynamicArray;
