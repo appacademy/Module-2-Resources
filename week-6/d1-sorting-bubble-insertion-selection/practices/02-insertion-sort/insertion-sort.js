5ae179338b3e91975f51f5b0653ff77cc3034db3
brandon <brandonlaursen95@yahoo.com>
2024-02-12 18:27:24 -0500

    insertion sort in place 2

--- week-6/d1-sorting-bubble-insertion-selection/practices/02-insertion-sort/insertion-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/02-insertion-sort/insertion-sort.js
@@ -57,41 +57,59 @@ function insertionSort(arr) {
 // In-place Insertion Sort
 // Mutates the original array
 
+// function insertionSortInPlace(arr) {
+//   // Set a pointer dividing the array into sorted and unsorted halves
+//   let divider = 1;
+
+//   // Repeat while the unsorted half is not empty:
+//   while (divider < arr.length) {
+//     // - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
+//     console.log(arr.join(","));
+
+//     // - Grab the first value from the unsorted half
+//     let val = arr[divider];
+
+//     // - For each value starting from the divider,
+//     let i = divider;
+//     while (i > 0) {
+//       // - Check if the value to the left is smaller than the unsorted value
+//       if (arr[i - 1] < val) {
+//         // - If so, you've reached the insertion point so exit the loop
+//         break;
+//       } else {
+//         // - If not shift the value to the right by 1 and continue
+//         arr[i] = arr[i - 1];
+//         i--;
+//       }
+//     }
+
+//     // - Insert the unsorted value at the break point
+//     arr[i] = val;
+//     // - Increment the dividing pointer and repeat
+//     divider++;
+//   }
+
+//   // Return the mutated array
+//   return arr;
+// }
+
+
 function insertionSortInPlace(arr) {
-  // Set a pointer dividing the array into sorted and unsorted halves
-  let divider = 1;
 
-  // Repeat while the unsorted half is not empty:
-  while (divider < arr.length) {
-    // - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
-    console.log(arr.join(","));
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
 
-    // - Grab the first value from the unsorted half
-    let val = arr[divider];
+    // ! this check makes it best case O(n)
+    // Will not enter while loop if loop is already sorted
+    while (j > 0 && arr[j] < arr[j - 1]) {
 
-    // - For each value starting from the divider,
-    let i = divider;
-    while (i > 0) {
-      // - Check if the value to the left is smaller than the unsorted value
-      if (arr[i - 1] < val) {
-        // - If so, you've reached the insertion point so exit the loop
-        break;
-      } else {
-        // - If not shift the value to the right by 1 and continue
-        arr[i] = arr[i - 1];
-        i--;
-      }
+      [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+      j--;
     }
-
-    // - Insert the unsorted value at the break point
-    arr[i] = val;
-    // - Increment the dividing pointer and repeat
-    divider++;
   }
-
-  // Return the mutated array
   return arr;
 }
+
 
 console.log(insertionSortInPlace([2, 4, 0, 8, 1]));
 
@@ -110,7 +128,7 @@ console.log(insertionSortInPlace([2, 4, 0, 8, 1]));
   [0, 2, 4, 8, 1] d: 2 val: 0 i: 0
   [0, 2, 4, 8, 1] d: 3 val: 8 i: 3
   [0, 1, 2, 4, 8] d: 4 val: 1 i: 1
-  [0, 1, 2, 4, 8] d: 5 
+  [0, 1, 2, 4, 8] d: 5
 
 */
 
