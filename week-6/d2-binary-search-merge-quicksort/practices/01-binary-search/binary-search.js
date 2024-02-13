32b7573767bda9b33817f06c6e5f22ba86117df4
brandon <brandonlaursen95@yahoo.com>
2024-02-13 12:33:33 -0500

    binary search

--- week-6/d2-binary-search-merge-quicksort/practices/01-binary-search/binary-search.js
+++ week-6/d2-binary-search-merge-quicksort/practices/01-binary-search/binary-search.js
@@ -1,6 +1,3 @@
-
-
-
 /*
 
   What is a Logarithm?
@@ -60,37 +57,53 @@
     * faster than O(n) - linear
 */
 
+function linearSearch(arr, target) {
+  // Can you solve this in one line ?
+  return arr.indexOf(target);
+}
 
-
-function linearSearch (arr, target) {
-
-  // Can you solve this in one line?
-
-};
+// console.log(linearSearch([2,4,6,8], 6));// 6
 
 function binarySearch(arr, target) {
-
   // Set integers pointing to the high and low range of possible indices
+  let low = 0;
+  let high = arr.length - 1;
 
   // While high and low indices do not overlap...
-
+  while (low <= high) {
     // Find the midpoint between high and low indices
-    // ! Math.floor
+    // ! Math.floor/ceil
+    let mid = Math.floor((low + high) / 2);
 
     // Compare the target value to the midpoint value
-
-    // If the target equals the midpoint...
+    if (arr[mid] === target) {
+      // If the target equals the midpoint...
       // Return the midpoint index
-
-    // If the target is higher than the midpoint...
+      return mid;
+    } else if (target > arr[mid]) {
+      // If the target is higher than the midpoint...
       // Move the low pointer to midpoint + 1
-
-    // If the target is less than the midpoint...
+      low = mid + 1;
+    } else {
+      // If the target is less than the midpoint...
       // Move the high pointer to midpoint - 1
+      hi = mid - 1;
+    }
+  }
 
   // Return -1 if the loop exits with overlapping pointers
+  return -1;
+}
 
-};
+console.log(binarySearch([2, 4, 6, 8], 6)); // 2
+console.log(binarySearch([2, 4, 6, 8, 9, 10, 12, 15], 10)); // 5
+/*
+
+  [2, 4, 6, 8, 9, 10, 12, 15]  Low: 0 | mid:0 | high: -1| target: 5
+
+    [2, 4, 6, 8, 9, 10, 12, 15]  Low: 0 | mid:0 | high: -1| target: 1
 
 
-module.exports = [linearSearch, binarySearch]
+*/
+
+module.exports = [linearSearch, binarySearch];
