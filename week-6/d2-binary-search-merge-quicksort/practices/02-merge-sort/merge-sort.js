f685a2f396de3ca2c5e8b215697c51c615c82745
brandon <brandonlaursen95@yahoo.com>
2024-02-13 12:40:14 -0500

    merge sort intro

--- week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
@@ -1,3 +1,34 @@
+/*
+
+
+
+   Merge sort
+   * Merge sort is the first of the efficient algorithms you will learn.
+
+   * Merge sort operates with a much better time complexity than the more naive approaches we started with.
+
+   The main ideas of merge sort are:
+    * 1. It is easy to merge elements of two sorted arrays into a single sorted array
+    * 2. You can consider an array containing only a single element as already sorted
+    * 3. You can also consider an empty array as sorted
+
+    Time Complexity
+     * Best case: O(n log n)
+     * Worst case: O(n log n)
+    Space complexity
+     * O(n) - proportional to size of call stack
+
+    This algorithm sorts values using the following divide and conquer approach:
+     * Split the unsorted array in half (divide)
+     * Sort the halves (conquer)
+     * Merge the newly sorted halves
+
+
+*/
+
+
+
+
 // Merge Sort out-of-place
 // Do not modify the original array
 function mergeSort(arr) {
