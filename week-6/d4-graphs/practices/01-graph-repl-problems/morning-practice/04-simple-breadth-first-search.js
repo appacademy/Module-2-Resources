5df2a15b8758443e87cce2d5843ef569d3369d0d
brandon <brandonlaursen95@yahoo.com>
2024-02-15 13:03:48 -0500

    start shortest path

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/04-simple-breadth-first-search.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/04-simple-breadth-first-search.js
@@ -8,9 +8,38 @@ const adjList = {
 }
 
 function breadthFirstSearch(start, end) {
-  // Your code here
-}
+// ! MEMORIZE
+  // * Create a queue and enqueue the starting node
+  const queue = [start];
 
+  // * Create a set to store visited nodes
+  const visitedNodes = new Set([start]);
+
+  // * While the queue is not empty, repeat steps 4-6
+  while (queue.length) {
+    // * Dequeue the first node
+    const currentNode = queue.shift();
+
+    // ? WHAT IS THE  THING THAT YOU NEED TO FOR THE DEQUEUED NODE
+    // console.log(currentNode);
+    // res.push(currentNode);// [ 3, 2, 4, 1, 5, 6 ]
+     if(currentNode === end) return  true;
+
+    // * For each unvisited neighbor,
+    // * add it to the visited nodes and to the back of the queue
+    let neighbors = adjList[currentNode];
+    // console.log(neighbors);
+
+    neighbors.forEach((neighbor) => {
+      if (!visitedNodes.has(neighbor)) {
+        visitedNodes.add(neighbor);
+        queue.push(neighbor);
+      }
+    });
+  };
+
+  return false;
+}
 console.log("First Test:");
 console.log(breadthFirstSearch(1, 3)); // -> true
 console.log("Second Test:");
